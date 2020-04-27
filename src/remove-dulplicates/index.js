383d4d3d60abfef39cbcb2dbe4fe030597349028
unknown <obrienules@gmail.com>
2020-04-27 16:17:06 +0100

    Complete Remove Duplicate, Run Tests

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,19 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    const newObj = {};
+    const arrOfKeys = Object.keys(obj).sort((a,b) => a + b).reverse();
+    let arrOfUniqueVals = []
+     
+    arrOfKeys.map((key) => {
+      let arr = [];
+      obj[key].map((value) => {
+        if (!arrOfUniqueVals.includes(value)) {
+          arr.push(value);
+        }
+       arrOfUniqueVals.push(value); 
+      });
+      newObj[key] = arr;
+    });
+  return newObj
+}
 
 module.exports = removeDuplicates;
