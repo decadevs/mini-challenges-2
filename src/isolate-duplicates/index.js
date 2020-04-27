ae8df32f97640f112168be1f6e4201c91c6196f6
engineer-kufre <engineer_kufre@yahoo.com>
2020-04-27 20:10:37 +0100

    solved isolate-duplicates

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,44 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+  if (typeof(text) !== 'string') {
+    throw "Please enter a valid string";
+  }
+
+  let result = [];
+  let count = 0;
+  let alphabet = text[0].toLowerCase();
+  let modifiedText = '';
+  let numberOfDuplicates = 0;
+
+  for(let i = 0; i < text.length; i++) {
+    if (alphabet === text[i].toLowerCase()) {
+      count++;
+      if (count === 3) {
+        modifiedText += '[' + text[i];
+        numberOfDuplicates++;
+      } else {
+        modifiedText += text[i];
+      }
+    } else if (alphabet !== text[i] && count > 2) {
+      alphabet = text[i].toLowerCase();
+      count = 1;
+      modifiedText += ']' + text[i];
+    } else {
+      alphabet = text[i].toLowerCase();
+      modifiedText += text[i];
+      count = 1;
+    }
+  }
+
+  if (count > 2) {
+    modifiedText += ']'
+  }
+
+  result.push(modifiedText);
+  result.push(numberOfDuplicates);
+
+  return result;
+}
 
 module.exports = isolateDuplicates;
+// console.log(isolateDuplicates());
+
