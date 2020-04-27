23ee1a1cac77a0232ca69614f6fdc575ccf3edf9
jbchinonso <nwekejohnbosco@gmail.com>
2020-04-27 20:53:10 +0100

    completed first trial code

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,22 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    const resultArray = []
+    let tmpString = ''
+    let tmpStringLength = tmpString.length
+    let duplicateString = []
+    let duplicateCharacter = ''
+   
+    for(let character of text){
+        
+        if(character == tmpString[tmpStringLength-1] && character == tmpString[tmpStringLength-2]){
+            tmpString += '[' + character
+            duplicateCharacter = character
+          }else if(duplicateCharacter !== character){
+              tmpString += ']' + character
+          }
+        tmpString+= character
 
+        }
+}
+
+console.log(isolateDuplicates("aaaabbcdefffffffg"))
 module.exports = isolateDuplicates;
