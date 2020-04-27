03f59147ffe57cf70306a60fb7d25e639483db97
Dexkode <dexkode4@gmail.com>
2020-04-27 21:41:06 +0100

    quarantine duplicates

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,44 @@
-function isolateDuplicates(text) {}
+// function isolateDuplicates(text) {
+//   let str = "";
+//   for (let i = 0; i < text.length; i++) {}
+// }
 
+// module.exports = isolateDuplicates;
+
+function isolateDuplicates(str) {
+  let tempArr = str.split("");
+  let letters = [];
+  let count = 1;
+  let freq = [];
+  let text = "";
+  for (let i = 0; i <= tempArr.length; i++) {
+    if (tempArr[i] === tempArr[i + 1]) {
+      count++;
+    } else {
+      let value = `${tempArr[i]}`;
+      letters = [...letters, value];
+      freq.push(count);
+      count = 1;
+    }
+  }
+
+  for (let i = 0; i < freq.length; i++) {
+    let str = "";
+    for (let j = 0; j < freq[i]; j++) {
+      str += letters[i];
+    }
+    if (str.length > 2) {
+      text += `${str.slice(0, 2)}[${str.slice(2)}]`;
+    } else {
+      text += str;
+    }
+  }
+  return new Array(text, freq.filter((num) => num > 2).length);
+}
+
+console.log(
+  isolateDuplicates(
+    "TttTTTTttttTTTTTTIIIIIiiiiiiiSSSSSSSSSTTTTAAAAAAAAAaaaaaaaaaSSSSSSSSSSKKKKKkkkkkk"
+  )
+);
 module.exports = isolateDuplicates;
