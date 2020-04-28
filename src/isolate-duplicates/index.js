34f743412d2c6e10432764cbf8e65b7bba2c7d72
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-04-28 23:47:53 +0100

    Morse test passed

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,40 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+  if (typeof text === "string") {
+    const uniqueText = String.prototype.concat(...new Set(text));
+
+    function addingBrackets(str) {
+      let output = [str.slice(0, 2), "[", str.slice(2), "]"].join("");
+      return output;
+    }
+
+    let newText = text.replace(/\w{3, }/g, (str) => addingBrackets(str));
+
+    console.log(newText);
+    // let numberOfDuplicatedLetters = 0;
+
+    // let arrayOfletters = [];
+
+    // let charMap = {};
+
+    // for (let letter of text) {
+    //   if (!charMap[letter]) {
+    //     charMap[letter] = 1;
+    //   } else {
+    //     charMap[letter]++;
+    //   }
+    // }
+
+    // for (let key in charMap) {
+    //   let number = charMap[key];
+    //   if (number > 2) {
+    //     numberOfDuplicatedLetters += 1;
+    //   }
+    // }
+
+    // return [JSON.stringify(arrayOfletters), numberOfDuplicatedLetters];
+  } else {
+    return "Please enter a valid string";
+  }
+}
 
 module.exports = isolateDuplicates;
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,53 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+  let arrayOfKeys = Object.keys(obj);
+  arrayOfKeys = arrayOfKeys.map((each) => parseInt(each, 10));
+  arrayOfKeys = arrayOfKeys.sort((a, b) => a - b);
+  arrayOfKeys = arrayOfKeys.map((each) => each.toString());
+
+  const reverseArrayOfKeys = arrayOfKeys.reverse();
+
+  let result = {};
+
+  // Pushing a Key:[] array to results
+  for (let each of arrayOfKeys) {
+    result[each] = [];
+  }
+
+  const arrayOfValues = [];
+
+  for (let key in obj) {
+    let values = obj[key];
+    for (let value of values) {
+      if (arrayOfValues.indexOf(value) < 0) {
+        arrayOfValues.push(value);
+      }
+    }
+  }
+
+  // Pushing each value from the array into the larger key in which it was previously present
+  for (let value of arrayOfValues) {
+    for (let key of reverseArrayOfKeys) {
+      let values = obj[key];
+      if (values.indexOf(value) >= 0) {
+        result[key].push(value);
+        break;
+      }
+    }
+  }
+
+  // Rearranging values to fit
+  for (let key in result) {
+    let objValues = obj[key];
+    let resultValues = result[key];
+    for (let index in objValues) {
+      if (resultValues.indexOf(objValues[index]) < 0) {
+        objValues.splice(index, 1);
+      }
+    }
+    result[key] = objValues;
+  }
+
+  return result;
+}
 
 module.exports = removeDuplicates;
