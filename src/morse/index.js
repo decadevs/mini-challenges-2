bbdee4ab41661489ae1cee1138f835e9dc4d5bd9
euchigere <ugochigere02@gmail.com>
2020-04-28 10:16:04 +0000

    morse solution

--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,36 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text) {
+  if (typeof text !== "string") {
+    throw "Please provide a morse string";
+  }
+
+  if (text === "") {
+    return "";
+  }
+
+  const TRIM_TEXT = text.trim();
+  let spaceCount = 0;
+  let characterCode = "";
+  let morseCode = "";
+
+  for (let i = 0; i < TRIM_TEXT.length; i++) {
+    if (TRIM_TEXT[i] !== " ") {
+      morseCode += TRIM_TEXT[i];
+      spaceCount = 0;
+    } else if ((spaceCount === 2) && (TRIM_TEXT[i] === " ")) {
+      characterCode += " ";
+      spaceCount = 0;
+    } else {
+      spaceCount++;
+      characterCode += morseCode? MORSE_CODE[morseCode] : "";
+      morseCode = "";
+    }
+  }
+
+  characterCode += MORSE_CODE[morseCode];
+  return characterCode;
+}
 
 module.exports = morse;
