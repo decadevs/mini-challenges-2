df78360854ec638ae06e59098703668dcbec6e0d
jbchinonso <nwekejohnbosco@gmail.com>
2020-04-28 13:10:51 +0100

    all test passed

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,38 +1,43 @@
 function isolateDuplicates(text) {
-    const resultArray = [];
-    let tmpString = "";
-    let tmpStringLength = tmpString.length;
-    let count = 0
-    let duplicateCharacter;
+  const resultArray = [];
+  let tmpString = "";
+  let tmpStringLength = tmpString.length;
+  let count = 0
+  let duplicateCharacter = [];
+  let lowerString
 
-    if(!text || typeof text !== 'string') return 'Please enter a valid string'
-  
-    for (let character of text) {
-      tmpStringLength = tmpString.length;
-      if (
-        character === tmpString[tmpStringLength - 1] &&
-        character === tmpString[tmpStringLength - 2]
-      ) {
-        if (tmpString.lastIndexOf("[") > tmpString.lastIndexOf("]")) {
-            console.log(tmpString.indexOf(character))
-          tmpString += character;
-          duplicateCharacter = character;
-        }else{
-            tmpString += "[" + character;
-            duplicateCharacter = character;
-            count++
-        }
-        
-      } else if (duplicateCharacter && duplicateCharacter !== character) {
-        tmpString += "]" + character;
-        duplicateCharacter = "";
-      }else{
-        tmpString += character;
-      }
+  if(!text) throw '' 
       
-    }
-  
-    resultArray.push(tmpString,count)
-    return resultArray
+  if(typeof text !== 'string')  throw 'Please enter a valid string'
+
+  const newText = text.toLowerCase()
+      
+  for (let i = 0; i < text.length; i++ ) {
+    tmpStringLength = tmpString.length;
+    lowerString = tmpString.toLowerCase()
+    
+    if (
+      newText[i] === lowerString[tmpStringLength - 1] &&
+      newText[i] === lowerString[tmpStringLength - 2]
+    ) {
+          duplicateCharacter.push(text[i])
+          
+         
+    }else if(newText[i] !== lowerString[tmpStringLength - 1] && duplicateCharacter.length > 0){
+        tmpString += "[" + duplicateCharacter.join('') + ']'+ text[i]
+        duplicateCharacter = []
+        count++
+    }else{
+      tmpString += text[i]
+    } 
+   
+    if(i === newText.length-1 && duplicateCharacter.length > 0){
+      tmpString += "[" + duplicateCharacter.join('') + ']'
+      duplicateCharacter = []
+      count++
+   }
   }
+  resultArray.push(tmpString,count)
+  return resultArray
+}
 module.exports = isolateDuplicates;
