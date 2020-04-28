1fbd3c36cd73b1a2b1dfd6d207b7b34a01b404cb
engineer-kufre <engineer_kufre@yahoo.com>
2020-04-28 03:29:59 +0100

    solved remove duplicates

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,26 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+  let strings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let keys = Object.keys(obj);
+  
+  let sortedObj = {};
+  keys.forEach(key => sortedObj[key] = obj[key])
+
+  for (let i = 0; i < strings.length; i++) {
+    let count = 0;
+    for (let j = keys.length - 1; j >= 0; j--) {
+      let k = 0;
+      while ( k < sortedObj[keys[j]].length) {
+        if (sortedObj[keys[j]][k] === strings[i] && count === 0) {
+          count++;
+        } else if (sortedObj[keys[j]][k] === strings[i] && count > 0) {
+          sortedObj[keys[j]].splice(k, 1);
+          k--;
+        }
+        k++;
+      }
+    }
+  }
+  return sortedObj;
+}
 
 module.exports = removeDuplicates;
