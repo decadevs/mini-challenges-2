6a52a8293af4d9cc09d2a5434ff8a380514dc4f1
Nwike Ugochukwu <bondesvick@gmail.com>
2020-04-28 23:32:11 +0100

    remove-duplplicates challange solved

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -3,28 +3,34 @@ function removeDuplicates(obj) {
     let theKeys = Object.keys(obj);
     let theValues = Object.values(obj);
     let count = 0
+    let noDuplicate = []
 
-    returned = Object.values(obj).reduce((final, element, i, arr) => {
-      final[theKeys[i]] = []
-
-      element.forEach((value, j, arr2) => {
-
-        for (let i = 0; i < theValues.length; i++) {
-          for (let j = 0; j < theValues[i].length; j++) {
-            if (theValues[i][j] === value) {
-              count += 1
-            }
+    theValues.forEach((element, i) => {
+      element.forEach(part => {
+        if (!noDuplicate.includes(part)) {
+          noDuplicate.push(part)
+        }
+      })
+    })
+    //noDuplicate
+    //theKeys
+    let returned = {}
+    Object.keys(obj).reverse().forEach(element => {
+      returned[element] = []
+      //console.log(obj[element])
+      obj[element].forEach((ler) => {
+        //console.log(ler)
+        noDuplicate.forEach((char, i) => {
+          //console.log(char)
+          if (ler == char && !(returned[element].includes(ler))) {
+            returned[element].push(noDuplicate[i])
+            noDuplicate[i] = null
           }
-        }
-        if (!(count > 1)) {
-          final[theKeys[i]].push(value)
-        }
-        count = 0
+        })
+
       });
-      theValues.splice(0, 1)
-      return final
-      
-    }, {})
+
+    })
 
     return returned;
 }
