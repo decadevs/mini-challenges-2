4af48e44dc92e182b03662f327cbedf99e7c572e
Adefabi Adekunle <adefabiyusuf@gmail.com>
2020-04-28 11:27:37 +0100

    commit1

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,36 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    if(typeof text==="string"){
+        let text1=[...text]
+        let arrayOfResult=[]
+        let arrayOfCharacter=text[0]+text[1]
+        let arrayOfDuplicate=""
+        let noOfArrayOfDuplicate=0
+        for(var i=2;i<text1.length;i++){
+          if(text1[i].toLowerCase()==arrayOfCharacter[arrayOfCharacter.length-1].toLowerCase()&&text1[i].toLowerCase()==arrayOfCharacter[arrayOfCharacter.length-2].toLowerCase()){
+              //arrayOfCharacter.push(arrayOfDuplicate)
+              arrayOfDuplicate+=text1[i]
+              arrayOfDuplicate
+             if(i<text1.length-1&&text1[i].toLowerCase()!==text1[i+1].toLowerCase()){
+                arrayOfCharacter+="["+arrayOfDuplicate+"]"
+                noOfArrayOfDuplicate++
+                arrayOfDuplicate=[]
+              }
+           }
+          else{
+            arrayOfCharacter+=text1[i]
+           }
+           console.log(arrayOfDuplicate)
+        }
+        noOfArrayOfDuplicate
+        console.log(arrayOfDuplicate)
+        if(arrayOfDuplicate.length>0){
+          noOfArrayOfDuplicate++
+        }
+        return [arrayOfCharacter,noOfArrayOfDuplicate]
+      }
+      else {
+        return "Please enter a valid string"
+      }
+}
 
 module.exports = isolateDuplicates;
