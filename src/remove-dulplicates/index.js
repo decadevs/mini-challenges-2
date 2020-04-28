f9b1ec8650858da9231a658326628f3c8ce6d088
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-04-28 22:21:14 +0100

    outline steps to approach remove duplicates task

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,49 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+
+    /* Steps to removing duplicates
+        1. Remove duplicate item from each Array
+            a. first define a function with parameter of the array to remove duplicate from, and a callback function
+            b. declare variable filteredArray  and arrayNoDuplicate, where later as an empty earray
+            c. loop through each item in the array using for loop
+                1. define a callback function with item of the array as parameter 
+                2. filter the array where each item of the array is same with the callback parameter
+                    1. if the length of the filteredArray is < 2; this means the current item checked using the callback has no duplicate
+                    2. else //we need to return the first item
+                        a. if the arrayNoDuplicate does not contain that item
+                        b. push it into arrayNoDuplicate
+            callback with the next array item as argument
+        
+        2. Keep keys of the obj in a seperate array and use it to get the array to be passed as parameter for removeDuplicate
+        3. Remove Intersect Items from Lower Level keys in the array
+            a.  define a variable removedIntersect
+            b.  Loop through the noDuplicateArray
+                1.  declare as emoty array- nonIntersect,othersNoDuplicate
+                2.  declare currentNoDuplicate as current array of the noDuplicateArray
+                3.  declare othersStartAtPos as current array position + 1
+                4.  if othersStartAtPos is equal to  length of noDuplcate; this means we have reached the end of the array and it will have no next array 
+                    a.  push the current array in removedIntersect
+                    else
+                    a.  while othersStartPos less than length of noDuplicate
+                        push the array to othersNoDuplicate
+                    b.  loop through currentNoDuplicate 
+                        1. loop through othersNoDuplicate
+                            a. if othersNoDuplicate does not contain currentNoDuplicate item
+                                1. increment count //count to show it has no intersection with that other Array
+                            b. if count is equal to othersNoDuplicate
+                                1. it means it has successfull traversed otherArray and yet no Intersection
+                                2. push the current item into the nonIntersect array  
+                push the nonIntersect into the removedIntersect
+            continue with next array item from the noDuplicateArray
+
+        4. loop through keys and make each of the array in removedIntersect the value
+
+        
+                
+                    
+
+
+
+    */
+}
 
 module.exports = removeDuplicates;
