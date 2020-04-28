806076e64f72f04b7c341d6073bc6a5410acbf0c
jbchinonso <nwekejohnbosco@gmail.com>
2020-04-28 09:58:20 +0100

    trying a second time

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,22 +1,38 @@
 function isolateDuplicates(text) {
-    const resultArray = []
-    let tmpString = ''
-    let tmpStringLength = tmpString.length
-    let duplicateString = []
-    let duplicateCharacter = ''
-   
-    for(let character of text){
-        
-        if(character == tmpString[tmpStringLength-1] && character == tmpString[tmpStringLength-2]){
-            tmpString += '[' + character
-            duplicateCharacter = character
-          }else if(duplicateCharacter !== character){
-              tmpString += ']' + character
-          }
-        tmpString+= character
+    const resultArray = [];
+    let tmpString = "";
+    let tmpStringLength = tmpString.length;
+    let count = 0
+    let duplicateCharacter;
 
+    if(!text || typeof text !== 'string') return 'Please enter a valid string'
+  
+    for (let character of text) {
+      tmpStringLength = tmpString.length;
+      if (
+        character === tmpString[tmpStringLength - 1] &&
+        character === tmpString[tmpStringLength - 2]
+      ) {
+        if (tmpString.lastIndexOf("[") > tmpString.lastIndexOf("]")) {
+            console.log(tmpString.indexOf(character))
+          tmpString += character;
+          duplicateCharacter = character;
+        }else{
+            tmpString += "[" + character;
+            duplicateCharacter = character;
+            count++
         }
-}
-
-console.log(isolateDuplicates("aaaabbcdefffffffg"))
+        
+      } else if (duplicateCharacter && duplicateCharacter !== character) {
+        tmpString += "]" + character;
+        duplicateCharacter = "";
+      }else{
+        tmpString += character;
+      }
+      
+    }
+  
+    resultArray.push(tmpString,count)
+    return resultArray
+  }
 module.exports = isolateDuplicates;
