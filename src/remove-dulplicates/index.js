112f674f6c16ec380c3459a8fa3d037e14260586
engineer-kufre <engineer_kufre@yahoo.com>
2020-04-28 03:40:05 +0100

    removed redundant code

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,26 +1,23 @@
 function removeDuplicates(obj) {
   let strings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let keys = Object.keys(obj);
-  
-  let sortedObj = {};
-  keys.forEach(key => sortedObj[key] = obj[key])
 
   for (let i = 0; i < strings.length; i++) {
     let count = 0;
     for (let j = keys.length - 1; j >= 0; j--) {
       let k = 0;
-      while ( k < sortedObj[keys[j]].length) {
-        if (sortedObj[keys[j]][k] === strings[i] && count === 0) {
+      while ( k < obj[keys[j]].length) {
+        if (obj[keys[j]][k] === strings[i] && count === 0) {
           count++;
-        } else if (sortedObj[keys[j]][k] === strings[i] && count > 0) {
-          sortedObj[keys[j]].splice(k, 1);
+        } else if (obj[keys[j]][k] === strings[i] && count > 0) {
+          obj[keys[j]].splice(k, 1);
           k--;
         }
         k++;
       }
     }
   }
-  return sortedObj;
+  return obj;
 }
 
 module.exports = removeDuplicates;
