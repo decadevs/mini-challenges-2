69a17f0359cbfd86ad9194785f04f892946230b4
Kaosilisochukwu Nwizu <50839130+Kaosilisochukwu@users.noreply.github.com>
2020-04-29 18:18:18 +0100

    optimize remove-duplicate function

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,6 +1,5 @@
 function removeDuplicates(obj) {
   let myKeys = Object.keys(obj);
-  let myValues = Object.values(obj);
   let keysHierarchy = myKeys.sort((a, b) => a - b);
   let compareArr = [];
   let myReturnObj = {};
@@ -8,27 +7,22 @@ function removeDuplicates(obj) {
     myReturnObj[element] = obj[element];
   });
   for (let i = keysHierarchy.length - 1; i >= 0; i -= 1) {
-    for (let j = 0; j < myReturnObj[keysHierarchy[i]].length; j += 1) {
-      if (!compareArr.includes(myReturnObj[keysHierarchy[i]][j])) {
-        compareArr.push(myReturnObj[keysHierarchy[i]][j]);
-      } else if (compareArr.includes(myReturnObj[keysHierarchy[i]][j])) {
-        myReturnObj[keysHierarchy[i]].splice(j, 1);
+    let objArr = myReturnObj[keysHierarchy[i]];
+
+    for (let j = 0; j < objArr.length; j += 1) {
+
+      let objArrItem = myReturnObj[keysHierarchy[i]][j];
+
+      if (!compareArr.includes(objArrItem)) {
+        compareArr.push(objArrItem);
+      } else if (compareArr.includes(objArrItem)) {
+        objArr.splice(j, 1);
         //console.log(myReturnObj[keysHierarchy[i]])
         j -= 1;
       }
     }
   }
-
-  return myReturnObj
+  return myReturnObj;
 }
-const obj = {
-  "350": ["C", "A", "A"],
-  "291": ["C", "C", "A", "A", "A", "C", "G"],
-  "236": ["L", "A", "X", "G", "H", "X"],
-  "91": ["P", "R", "S", "D"],
-  "53": ["L", "G", "B", "C"],
-  "12": ["A", "A", "B", "D"],
-};
-
 
 module.exports = removeDuplicates;
