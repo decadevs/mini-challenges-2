b2bf717f96988a632d073da9303833ebb367fa10
teeliny <teeliny@gmail.com>
2020-04-29 16:50:06 +0100

    Changes to index.js files of morse and isolate duplicates

--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,42 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text) {
+  if (typeof text !== 'string') {
+    throw ('Please provide a morse string');
+  }
+  else {
+    let morseWords = text.trim().split('   ');
+    let morseChar = [];
+    for (let i = 0; i < morseWords.length; i++) {
+      morseChar.push(morseWords[i].split(' '));
+    }
+
+    let eachWord = [];
+    for (let i = 0; i < morseChar.length; i++) {
+      let eachChar = '';
+      for (let j = 0; j < morseChar[i].length; j++) {
+        let char = morseChar[i][j];
+        if (char in MORSE_CODE) {
+          eachChar += (MORSE_CODE[char]);
+        }
+      }
+      eachWord.push(eachChar);
+    }
+
+    let textLength = text.length;
+    let finalWord = eachWord.join(' ');
+
+    let validMorse = morseWords[0][0] in MORSE_CODE;
+
+      if (textLength === 0) {
+        return "";
+      }
+      else if (validMorse == true) {
+        return finalWord;
+      }
+  }
+
+}
 
 module.exports = morse;
