954530cc1ffafd72f72490bd56041951a3d4109a
Oladokun Oladapo <oladokun104@gmail.com>
2020-04-29 13:25:58 +0100

    Descriptive Comments added

--- src/morse/index.js
+++ src/morse/index.js
@@ -61,16 +61,20 @@ Object.freeze(MORSE_CODE);
 function morse(text) {
   if (typeof text === "string") {
     let convertedArr = [];
+    // Create an Array from the Morse String and Split by the spaces
     let arrFromMorse = Array.from(text.split(" "));
 
     for (let i = 0; i < arrFromMorse.length; i++) {
+      // Check and delete empty string from the array
       if (arrFromMorse[i] !== "") {
         convertedArr.push(MORSE_CODE[arrFromMorse[i]]);
+        // Check if there's more than one empty string and add a space if true.
         if (arrFromMorse[i + 1] === "" && arrFromMorse[i + 2] === "") {
           convertedArr.push(" ");
         }
       }
     }
+    // Remove extra space at the end of the converted array
     if (convertedArr[convertedArr.length - 1] === " ") {
       convertedArr.pop();
     }
