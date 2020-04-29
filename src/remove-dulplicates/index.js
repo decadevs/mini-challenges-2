69e64ff32412f824decde0efcef591923464a8f3
Kaosilisochukwu Nwizu <50839130+Kaosilisochukwu@users.noreply.github.com>
2020-04-29 18:04:01 +0100

    implement remove-duplicate function

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,34 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+  let myKeys = Object.keys(obj);
+  let myValues = Object.values(obj);
+  let keysHierarchy = myKeys.sort((a, b) => a - b);
+  let compareArr = [];
+  let myReturnObj = {};
+  keysHierarchy.forEach((element) => {
+    myReturnObj[element] = obj[element];
+  });
+  for (let i = keysHierarchy.length - 1; i >= 0; i -= 1) {
+    for (let j = 0; j < myReturnObj[keysHierarchy[i]].length; j += 1) {
+      if (!compareArr.includes(myReturnObj[keysHierarchy[i]][j])) {
+        compareArr.push(myReturnObj[keysHierarchy[i]][j]);
+      } else if (compareArr.includes(myReturnObj[keysHierarchy[i]][j])) {
+        myReturnObj[keysHierarchy[i]].splice(j, 1);
+        //console.log(myReturnObj[keysHierarchy[i]])
+        j -= 1;
+      }
+    }
+  }
+
+  return myReturnObj
+}
+const obj = {
+  "350": ["C", "A", "A"],
+  "291": ["C", "C", "A", "A", "A", "C", "G"],
+  "236": ["L", "A", "X", "G", "H", "X"],
+  "91": ["P", "R", "S", "D"],
+  "53": ["L", "G", "B", "C"],
+  "12": ["A", "A", "B", "D"],
+};
+
 
 module.exports = removeDuplicates;
