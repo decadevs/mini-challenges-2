4cc9410de16526a620238cecd463221f7ddd4446
Uchey <ucheuzochukwumark@gmail.com>
2020-04-29 21:24:39 +0100

    commit rmv-duplicate

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,25 @@
-function removeDuplicates(obj) {}
+const removeDuplicates = (obj) => {
+    let checkValueArr = []
+    const resultObj = {};
+let objKey = Object.keys(obj).sort((a,b) => a + b).reverse();
+    
+objKey.map(key => {
+    let arr = [];
+    obj[key].map(value => {
+        if(!checkValueArr.includes(value)){
+            arr.push(value);
+        }
+        checkValueArr.push(value)
+    });
+    resultObj[key] = arr
+});
+   return resultObj
+};
 
+// const obj = {
+//     "1": ["C", "F", "G"],
+//     "2": ["A", "B", "C"],
+//     "3": ["A", "B", "D"],
+//   };
+//   console.log(removeDuplicates(obj));
 module.exports = removeDuplicates;
