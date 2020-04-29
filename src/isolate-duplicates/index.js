97b26b2c250ea6554fa8b57c8db225e6fda5a782
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-04-29 20:06:47 +0100

    Isolate-duplicates test passed

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,39 +1,14 @@
 function isolateDuplicates(text) {
   if (typeof text === "string") {
-    const uniqueText = String.prototype.concat(...new Set(text));
-
-    function addingBrackets(str) {
-      let output = [str.slice(0, 2), "[", str.slice(2), "]"].join("");
-      return output;
-    }
-
-    let newText = text.replace(/\w{3, }/g, function (letters) {
+    // Finding repeated character sequences and inputing [] after the second index
+    let newText = text.replace(/(\S)(\1{2,})/gi, function (letters) {
       return [letters.slice(0, 2), "[", letters.slice(2), "]"].join("");
     });
 
-    console.log(newText);
-    // let numberOfDuplicatedLetters = 0;
+    // Counting the number of duplicates
+    const numberOfDuplicates = (newText.match(/\[/g) || []).length;
 
-    // let arrayOfletters = [];
-
-    // let charMap = {};
-
-    // for (let letter of text) {
-    //   if (!charMap[letter]) {
-    //     charMap[letter] = 1;
-    //   } else {
-    //     charMap[letter]++;
-    //   }
-    // }
-
-    // for (let key in charMap) {
-    //   let number = charMap[key];
-    //   if (number > 2) {
-    //     numberOfDuplicatedLetters += 1;
-    //   }
-    // }
-
-    // return [JSON.stringify(arrayOfletters), numberOfDuplicatedLetters];
+    return [newText, numberOfDuplicates];
   } else {
     throw "Please enter a valid string";
   }
