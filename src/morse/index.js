f410fb83310b73561e5220cbab21b1ce7311d059
engrceey <ogbondachristian@gmail.com>
2020-04-29 21:13:56 +0000

    final solution

--- src/morse/index.js
+++ src/morse/index.js
@@ -71,12 +71,12 @@ function morse(text) {
           arrWords[i] = arrWords[i].split(" ");
       for (let j = 0; j < arrWords[i].length; j++) {
         arrWords[i][j] = MORSE_CODE[arrWords[i][j]];
-        console.log(arrWords[i][j]);
+
         }
       }
       for (let i = 0; i < arrWords.length; i++) {
         arrWords[i] = arrWords[i].join("");
-        console.log(arrWords[i]);
+
         }
         return arrWords.join(" ");
     }
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,27 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    let keyArr = [];
+for (let keys in obj) {
+  keyArr.push(keys);
+}
+
+let sortedArr = keyArr.reverse();
+
+let checker = [];
+let newObj = {};
+let objArr = [];
+
+for (let i = 0; i < sortedArr.length; i++) {
+  for (let j = 0; j < obj[sortedArr[i]].length; j++) {
+    if (!checker.includes(obj[sortedArr[i]][j])) {
+      checker.push(obj[sortedArr[i]][j]);
+      objArr.push(obj[sortedArr[i]][j]);
+    }
+  }
+  newObj[sortedArr[i]] = objArr;
+  objArr = [];
+}
+
+return newObj;
+}
 
 module.exports = removeDuplicates;
