e86c3de17f19f4fba98acc53f481430b12ca9139
Daniel Otulagun <Danotsonof@gmail.com>
2020-04-29 12:25:24 +0100

    adding my solution codes to challenge

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,26 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+	if (text === '' || typeof(text) !== 'string') {
+		throw new Error("Please enter a valid string");
+	}
+	let str = ''
+	let count = 0
+	let copyText = text.slice()
+	for (let i = 0; i < text.length;) {
+		let pattern = '['+text[i]+']+'
+		let regex = new RegExp(pattern, 'i');
+		let match = copyText.match(regex)[0]
+		let matchLen = match.length
+		if(matchLen < 3) {
+			str += match
+		} else {
+			++count
+			str += match.slice(0,2)
+			str += `[${match.slice(2,)}]`
+		}
+		i += match.length
+		copyText = copyText.slice(match.length)
+	}
+	return [str, count]
+}
 
 module.exports = isolateDuplicates;
