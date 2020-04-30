5645bd36ab335d49e12be589a20094bf92666dac
Olatunji Oye <oyeolatunji@gmail.com>
2020-04-30 16:57:35 +0100

    remove-duplicates-updated

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -3,40 +3,35 @@ function removeDuplicates(obj) {
     let keys = Object.keys(obj);
     let sorted = keys.sort((a, b) => a - b);
     let values = Object.values(obj);
-    let flattenedArr = values.flat();
-    let uniqueFlat = getUnique(flattenedArr);
+    control = [];
     
     for (let i = values.length-1; i > -1; i--){
         
+        let temp = [];
         let uniqArr = getUnique(values[i])
         values[i] = uniqArr;
-        if (i != -1){
-            for (let j = 0; j < values[i].length; j++){
-        
-                if (uniqueFlat.length == 0){
-                    values[i].splice(j, 1);
-                }
-                else {
-                    for (let x = 0; x < uniqueFlat.length; x++){
-                        if (values[i][j] == uniqueFlat[x]){
-                            uniqueFlat.splice(x, 1);
-                            x = uniqueFlat.length-1
-                        }
-                        else{
-                            values[i].splice(j, 1);
-                        }
-                    }
-                }
+        let len = values[i].length;
+        for (let j = 0; j < len; j++){
 
-            }            
+            let string = checkControl(values[i][j], control)
+            if (string != ''){
+                temp.push(string);
+            }
+
+        }
+
+        values[i] = temp;
+        for (let x = 0; x < temp.length; x++){
+            control.push(temp[x]);
         }
 
     }
-    console.log(values, uniqueFlat);
-    let result = Object.prototype;
+    
+    let result = {};
     for (let i = 0; i < values.length; i++){
         result[sorted[i]] = values[i];
     }
+
     return result;
 }
 
@@ -45,10 +40,24 @@ function getUnique(arr){
     return unique;
 }
 
-module.exports = removeDuplicates;
+function checkControl(string, controlArr){
+    let check = false;
+    for (let k = 0; k < controlArr.length; k++){
+        if (string == controlArr[k]){
+            check = true;
+            break;
+        }
+        else {
+            check = false
+        }
+    }
+    if (check == false){
+        return string;
+    }
+    else {
+        return '';
+    }
+}
 
-console.log(removeDuplicates({
-    "1": ["A"],
-    "2": ["A"],
-    "3": ["A"],
-  }));
+
+module.exports = removeDuplicates;
