3363474eb203e135fd46eda90e49642f4373b4ba
anabiz <anabizconcept9@gmail.com>
2020-04-30 13:34:39 +0100

    update index.js/morse
    update solution to the morse challenge.

--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,33 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text) {
+    if((typeof text ) != "string"){
+    throw "Please provide a morse string";
+  }
+  text = text.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+  let toArr= text.split(""),
+  morseChar="",
+  character ="",
+  word=[];
+  for(let a=0; a<toArr.length; a++){
+    if(toArr[a] != " "){
+      morseChar += toArr[a];
+    }
+    if(toArr[a]==" " && toArr[a+1] != " "){
+      character += MORSE_CODE[morseChar];
+      morseChar = "";
+    }
+    if(toArr[a]==" " && toArr[a+1]==" " && toArr[a+2]==" " || a==toArr.length-1){
+      character += MORSE_CODE[morseChar];
+      word.push(character);
+      character="";
+      morseChar = "";
+      a +=2;
+    }
+  }
+  return word.toString().replace(/,/g," ");
+
+}
 
 module.exports = morse;
