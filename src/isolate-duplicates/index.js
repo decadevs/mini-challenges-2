98a950e36b4442f32cd20b8d265d276d20c04210
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-05-11 08:42:56 -0700

    finished isolate-duplicates

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -2,17 +2,21 @@ function isolateDuplicates(text) {
     let counter = 0;
     let newString = "";
     let duplicates = 0;
+    if (typeof text !== "string") throw new TypeError("Please enter a valid string");
     for (let i = 0; i < text.length; i++ ) {
         if (i > 0 && text[i].toLowerCase() === text[i - 1].toLowerCase()) counter++;
+        if (i > 0 && text[i].toLowerCase() !== text[i - 1].toLowerCase() && counter >= 2) {
+            counter = 0;
+            newString += "]";
+        }
         if (counter === 2) {
             newString += "[";
             duplicates++;
         }
-        if (i > 0 && text[i].toLowerCase() !== text[i - 1].toLowerCase() && counter > 2) {
-            counter = 0;
-            newString += "]";
-        }
+        
         if (i > 0 && text[i].toLowerCase() !== text[i - 1].toLowerCase() && counter > 0) counter = 0;
+
+        // console.log(text[i], counter)
         newString += text[i];
 
     }
@@ -21,10 +25,6 @@ function isolateDuplicates(text) {
     return [newString, duplicates];
 }
 
-
-// console.log(isolateDuplicates("HelllllooooooDeeeecadevvsss"));
-// console.log(isolateDuplicates("HEEEELLLLLOOOOOOOTHHHHHEEEEERRRREEE"));
-// console.log(isolateDuplicates("boopdedoop"));
-// console.log(isolateDuplicates("TttTTTTttttTTTTTTIIIIIiiiiiiiSSSSSSSSSTTTTAAAAAAAAAaaaaaaaaaSSSSSSSSSSKKKKKkkkkkk"));
+console.log(isolateDuplicates("helloookat"));
 
 module.exports = isolateDuplicates;
