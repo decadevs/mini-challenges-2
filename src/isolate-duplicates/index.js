50ca4afd1a7a529a06cf0e1df37e3b1869789e7e
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-05-11 08:25:40 -0700

    initial commit

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,30 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    let counter = 0;
+    let newString = "";
+    let duplicates = 0;
+    for (let i = 0; i < text.length; i++ ) {
+        if (i > 0 && text[i].toLowerCase() === text[i - 1].toLowerCase()) counter++;
+        if (counter === 2) {
+            newString += "[";
+            duplicates++;
+        }
+        if (i > 0 && text[i].toLowerCase() !== text[i - 1].toLowerCase() && counter > 2) {
+            counter = 0;
+            newString += "]";
+        }
+        if (i > 0 && text[i].toLowerCase() !== text[i - 1].toLowerCase() && counter > 0) counter = 0;
+        newString += text[i];
+
+    }
+
+    newString += counter >= 2 ? "]" : "";
+    return [newString, duplicates];
+}
+
+
+// console.log(isolateDuplicates("HelllllooooooDeeeecadevvsss"));
+// console.log(isolateDuplicates("HEEEELLLLLOOOOOOOTHHHHHEEEEERRRREEE"));
+// console.log(isolateDuplicates("boopdedoop"));
+// console.log(isolateDuplicates("TttTTTTttttTTTTTTIIIIIiiiiiiiSSSSSSSSSTTTTAAAAAAAAAaaaaaaaaaSSSSSSSSSSKKKKKkkkkkk"));
 
 module.exports = isolateDuplicates;
