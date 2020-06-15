0c99ce8cc2b599f3cba7d5c73d630b4201fbf5a1
Uchey <ucheuzochukwumark@gmail.com>
2020-06-15 23:28:05 +0100

    refactor isolate duplicate

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,23 +1,21 @@
-const isolateDuplicates = (text) => {
-    if (typeof text !== 'string' ) throw new Error ("Please enter a valid string");
-    const matchRepeatedString = text.match(/(.)\1{0,}/gi);
-   
-    let result = '';
-    let counter = 0;
-    let firstTwoChar;
-    let remChar;
-    matchRepeatedString.map(char => {
-      if(char.length > 2){
-        counter++
-        firstTwoChar = char.substr(0, 2);
-        remChar = char.substr(2);
-        result += `${firstTwoChar}[${remChar}]`
-      }else{
-        result += char
-      }
-    });
-   return [result, counter]
-}
+const isolateDuplicates = text => {
+  if (typeof text !== 'string') throw Error("Please enter a valid string");
+  const mapRepeatedString = text.match(/(.)\1{0,}/gi);
+  let result = '';
+  let counter = 0;
 
-// console.log(isolateDuplicates())
+  mapRepeatedString.map(char => {
+    if (char.length > 2) {
+      counter++
+      const firstTwoChars = char.substr(0, 2);
+      const remChars = char.substr(2);
+      result += `${firstTwoChars}[${remChars}]`
+    } else {
+      result += char
+    }
+  });
+
+  return [result, counter]
+};
+
 module.exports = isolateDuplicates;
