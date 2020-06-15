baec0800d3f768a7c9ac4a0036548c8566edb7c0
Uchey <ucheuzochukwumark@gmail.com>
2020-06-15 23:44:22 +0100

    complete week 1 task code rebase

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,25 +1,21 @@
-const removeDuplicates = (obj) => {
-    let checkValueArr = []
-    const resultObj = {};
-let objKey = Object.keys(obj).sort((a,b) => a + b).reverse();
-    
-objKey.map(key => {
+const removeDuplicates = obj => {
+  let checkValueArr = []
+  const resultObj = {};
+  let sortedObjKey = Object.keys(obj).sort((a, b) => a + b).reverse();
+
+  sortedObjKey.map(key => {
     let arr = [];
+
     obj[key].map(value => {
-        if(!checkValueArr.includes(value)){
-            arr.push(value);
-        }
-        checkValueArr.push(value)
+      if (!checkValueArr.includes(value)) {
+        arr.push(value);
+      }
+      checkValueArr.push(value)
     });
     resultObj[key] = arr
-});
-   return resultObj
+  });
+
+  return resultObj
 };
 
-// const obj = {
-//     "1": ["C", "F", "G"],
-//     "2": ["A", "B", "C"],
-//     "3": ["A", "B", "D"],
-//   };
-//   console.log(removeDuplicates(obj));
 module.exports = removeDuplicates;
