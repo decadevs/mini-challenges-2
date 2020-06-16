6b7c63253427138a6134de9a8f6b64aede243738
deyems <adeyemisola11@gmail.com>
2020-06-17 00:51:26 +0100

    Using regex to finding alphabet occurence greater than 2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,27 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+  if(text === undefined || typeof text !== "string") {
+    throw Error("Please enter a valid string");
+  }
+
+  //FIND REPEATED PATTERNS
+  let regex = /(.)\1*/gi;
+
+  //STORE THE REPEATED LETTERS UNIQUELY
+  const groupedLetters = text.match(regex);
+  const bound = 2;
+  let newString = ""; let trackRepeat = 0;
+
+  for(letters of groupedLetters){
+    if(letters.length>bound){
+      newString += letters.slice(0,bound).concat("[").concat(letters.slice(bound)).concat("]");
+      trackRepeat++;
+    }else{
+      newString += letters;
+    }
+  }
+
+  return [newString,trackRepeat];
+
+}
 
 module.exports = isolateDuplicates;
