985205183a427991943b91edbf134d305d91daa3
Abdullahi Onaolapo <onaolapo.ak@gmail.com>
2020-06-17 21:08:41 +0100

    remove duplicates

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,21 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+
+    let arrayOfKeys = Object.keys(obj).slice();
+    let valueDetector = []
+    const result = {};
+    let objKey = arrayOfKeys.sort((a,b) => a + b).reverse();
+    
+    objKey.map(key => {
+    let arr = [];
+    obj[key].map(currentValue => {
+        if(!valueDetector.includes(currentValue)){
+            arr.push(currentValue);
+        }
+        valueDetector.push(currentValue)
+    });
+    result[key] = arr
+});
+   return result
+}
 
 module.exports = removeDuplicates;
