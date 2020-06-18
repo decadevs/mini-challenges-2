d69bdefc132ca229cd1eaf8cdc90fc338b42d98e
tosincoker26 <tosincoker26@gmail.com>
2020-06-18 20:46:47 +0100

    Solutions to Remove Duplicates

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,26 @@
-function isolateDuplicates(text) {}
+function finalStringuplicates(text) {
+  if(typeof text !== 'string' ) throw new Error("Please enter a valid string");
 
-module.exports = isolateDuplicates;
+  let counter = 0
+  let finalString = '';
+  let arrOfRepeatedString = text.match(/([a-z])\1{0,}/gi);
+
+
+  for(let word of arrOfRepeatedString){
+      if(word.length > 2){
+          counter+= 1;
+          let remLetters = word.split('').splice(2).join('');
+          let firstTwoLetters = word.substring(0,2);
+
+          finalString += `${firstTwoLetters}[${remLetters}]`
+
+      }else{
+        finalString += word
+      }
+    }
+
+    return [finalString, counter]
+
+}
+
+module.exports = finalStringuplicates;
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,21 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+  let arrayOfKeys = Object.keys(obj);
+  let tempHolder = []
+  const output = {};
+  let objKey = arrayOfKeys.sort((a,b) => a - b).reverse();
+
+  objKey.map(key => {
+    let newArr = [];
+    obj[key].map(currentValue => {
+        if(!tempHolder.includes(currentValue)){
+            newArr.push(currentValue);
+        }
+        tempHolder.push(currentValue)
+    });
+    output[key] = newArr
+});
+   return output
+
+}
 
 module.exports = removeDuplicates;
