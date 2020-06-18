2643c06dbd5826a68859fa3749b2bd177fd4305d
daphycisist <daphycisist@gmail.com>
2020-06-18 11:57:10 +0100

    add isolate-duplicates solution

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,35 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+  if (typeof (text) !== 'string')
+    throw Error("Please enter a valid string");
+
+  let letterCount = 1;
+  let arrayCount = 0;
+  let answer = []
+  const textArr = text.split('');
+  const textArrCopy = text.toLowerCase().split('');
+
+  for (let i = 0; i < textArr.length; i++) {
+    if (textArrCopy[i] === textArrCopy[i + 1]) {
+
+      letterCount++;
+      answer.push(textArr[i])
+      if (letterCount === 3) answer.push('[')
+
+    } else {
+
+      answer.push(textArr[i]);
+      if (letterCount > 2) {
+        answer.push(']')
+        arrayCount++;
+        
+      }
+      letterCount = 1;
+    }
+  }
+  return [answer.join(''), arrayCount];
+}
+
+
+console.log(isolateDuplicates('aaaabbcdefffffffg'));
 
 module.exports = isolateDuplicates;
