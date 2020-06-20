b8325b5aebf073d52a46f63be4d6b3d2e3f9ebc9
thoraf20 <50268671+thoraf20@users.noreply.github.com>
2020-06-20 08:06:19 +0100

    updated challenge 2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,50 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
 
-module.exports = isolateDuplicates;
+    if (typeof text !== 'string'){
+        throw new Error('Please enter a valid string')
+    };
+    
+    let controlArray = [];
+    let arrayOfResult = [];
+    let counter = 0;
+    let arrayOfUpperCasedText = text.toUpperCase().split('');
+    text = text.split('');
+    for (let i = 0; i < text.length; i++) {
+        if(arrayOfUpperCasedText[i] !== arrayOfUpperCasedText[i + 1]) controlArray.push([text[i]]);
+    }
+    
+    const fillHashTable = (array, correlation, juxtaposeCharacter) => {
+        let shelterArray = [];
+        for (let j = 0; j < array.length; j++) {
+            if(correlation[j] !== correlation[j + 1]) break;
+            if(correlation[j] === correlation[j+1] && correlation[j] === juxtaposeCharacter.toUpperCase()) {
+               shelterArray.push(array[j])
+            }
+        }
+        let last = array[shelterArray.length];
+        shelterArray.push(last);
+        for (let k = 0; k <= shelterArray.length - 1; k++) {
+            array.shift(juxtaposeCharacter);
+            correlation.shift(shelterArray[k]);
+         }
+        return shelterArray;
+    }
+    
+    for (let l = 0; l < controlArray.length; l++){
+    controlArray[l] = [...controlArray[l].concat(fillHashTable(text, arrayOfUpperCasedText, controlArray[l][0]))]
+    controlArray[l].splice(0, 1);
+    if(controlArray[l].length > 2){
+        controlArray[l].splice(controlArray[l].length, 0, ']');
+        controlArray[l].splice(2, 0, '[');
+        counter++
+
+    }
+        arrayOfResult = arrayOfResult.concat(controlArray[l]);
+    }
+    return [arrayOfResult.join(''), counter];
+
+}
+
+
+module.exports = isolateDuplicates
+
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,29 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+
+    let arrayOfKeys = Object.keys(obj).slice(); // copy all the keys in the input object
+
+    let referenceKey = []; 
+
+    const output = {};
+
+    let sortedKeys = arrayOfKeys.sort((a,b) => a + b).reverse(); // sort the copied keys and reverse the order
+    
+    sortedKeys.map(key => {
+
+    let arr = [];
+
+    obj[key].map(Value => {//access the value of each key in the input object
+
+        if(!referenceKey.includes(Value)){//look up each value in referenceKey
+
+            arr.push(Value);//store the obj key values not in referenceKey
+        }
+        referenceKey.push(Value) // update referenceKey array
+    });
+    output[key] = arr
+});
+   return output;
+}
 
 module.exports = removeDuplicates;
+
