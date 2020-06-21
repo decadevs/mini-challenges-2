ce1b8f283ed5ef0daf2d23ce1c3e8deb541f33f6
warami13 <wrmyeresanara@gmail.com>
2020-06-21 01:02:54 +0100

    add solution for isolate-duplicates

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -10,44 +10,38 @@ Pseudocode
 - if the the index in the array is === to the previous text and the previous two texts then it should be pushed into the duplicate arr
 	- but if the index in the array isn't equal to the previous text then it should open the new arr to hold the duplicates if there are any
 	- count the duplicates
-- edge cases
-	- change all the texts to lowercase
 */
 function isolateDuplicates(text) {
+    console.log(typeof text === "string");
     if (typeof text !== "string") throw "Please enter a valid string";
-
     let newStr = '';
     let finalArr = [];
     let duplicateArr = [];
     let myCount = 0;
-
     for (let i = 0; i < text.length; i++) {
-        let currentTxt = text[i].toLowerCase();
+        let currentTxt = text[i];
         let previousTxt = text[i - 1];
         let previous2Txt = text[i - 2];
-
-        if (currentTxt === previousTxt && currentTxt === previous2Txt) {
+        if (currentTxt.toLowerCase() === (previousTxt && previousTxt.toLowerCase()) && currentTxt.toLowerCase() === (previous2Txt && previous2Txt.toLowerCase())) {
             duplicateArr.push(currentTxt);
-        } else if (currentTxt !== previousTxt && duplicateArr.length > 0) {
+
+        } else if ((currentTxt && currentTxt.toLowerCase()) !== (previousTxt && previousTxt.toLowerCase()) && duplicateArr.length > 0) {
             newStr += '[' + duplicateArr.join('') + ']' + currentTxt
             duplicateArr = [];
             myCount++;
-        } else newStr += currentTxt;
+        } else {
+            newStr += currentTxt;
+        }
         // if the last letter in the text is a duplicate;
         if (text.length - 1 === i && duplicateArr.length > 0) {
-
             newStr += '[' + duplicateArr.join('') + ']'
             duplicateArr = [];
             myCount++;
         }
-
     }
-
     finalArr.push(newStr, myCount);
+    // console.log(duplicateArr);
     return finalArr;
-
 }
-
-
 
 module.exports = isolateDuplicates;
