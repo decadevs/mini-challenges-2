2317bf36111e3d8bdb6a53ab142204b88807aba9
Dexkode <dexkode4@gmail.com>
2020-06-29 18:37:42 +0100

    isolate week2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,35 +1,22 @@
 function isolateDuplicates(text) {
   if (typeof text !== "string") {
-    return "Please enter a valid string";
+    throw "Please enter a valid string";
   }
-  // let text = text.split("");
-  let letters = [];
-  let count = 1;
-  let freq = [];
-  let res = "";
 
-  for (let i = 0; i <= text.length; i++) {
-    if (text[i] === text[i + 1]) {
-      count++;
-    } else {
-      let value = `${text[i]}`;
-      letters = [...letters, value];
-      freq.push(count);
-      count = 1;
-    }
-  }
-  for (let i = 0; i < freq.length; i++) {
-    let text = "";
-    for (let j = 0; j < freq[i]; j++) {
-      text += letters[i];
-    }
+  let duplicates = text.match(/(.)\1{0,}/gi);
+  let res = "";
+  let count = 0;
+  for (let i = 0; i < duplicates.length; i++) {
+    let text = duplicates[i];
     if (text.length > 2) {
+      count++;
       res += `${text.slice(0, 2)}[${text.slice(2)}]`;
     } else {
       res += text;
     }
   }
-  return new Array(res, freq.filter((num) => num > 2).length);
+
+  return [res, count];
 }
 
 module.exports = isolateDuplicates;
