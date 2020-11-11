70ed8d5e765478376612a1c9bbd90a33560ba8f3
Anthony Benarivo <benarivo66@gmail.com>
2020-11-11 06:01:59 +0100

    for loop

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,10 +1,18 @@
 function isolateDuplicates(text) {
+    let arr = text.split(''); 
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (newArr.length === 0) {
+            newArr.push(arr[0]);
+        } else if (newArr[newArr.length-1] !== arr[i]) {
+            newArr.push(arr[i]);
+        } else if (newArr[newArr.length-1] === arr[i]) {
+
+        }
+    } 
+}
+
     
-}
-function checkString(txt) {
-    if (typeof txt !== 'string') {
-        return "Please enter a valid string";
-    }
-}
+
 
 module.exports = isolateDuplicates;
