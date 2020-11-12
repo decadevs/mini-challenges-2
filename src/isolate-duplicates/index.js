25113bb773cd15fd60f667f5de2734ce26a9a351
Anthony Benarivo <benarivo66@gmail.com>
2020-11-12 03:09:45 +0100

    split array into chunks of same letters

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,15 +1,20 @@
 function isolateDuplicates(text) {
-    let arr = text.split(''); 
-    let newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (newArr.length === 0) {
-            newArr.push(arr[0]);
-        } else if (newArr[newArr.length-1] !== arr[i]) {
-            newArr.push(arr[i]);
-        } else if (newArr[newArr.length-1] === arr[i]) {
-
+    let arr = text.split("");
+    let newArr = [...new Set(arr)];
+    
+    return newArr.map(txt => txt.repeat(count(txt)));
+    
+    function count(chr) {
+        let str = text;
+        let strl = str.length;
+        for (i = l = 0; i < strl; i++) {
+            if (str[i] === chr) {
+                l++;
+            }
         }
-    } 
+        return l;
+    }
+
 }
 
     
