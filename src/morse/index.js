bf84d80136a2530fd5fd2beac69d283175fb4431
Anthony Benarivo <benarivo66@gmail.com>
2020-11-12 12:50:18 +0100

    split, iterate, push, join

--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,13 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text) {
+  let arr = text.split('');
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(MORSE_CODE(arr[i]));    
+  }
+  return newArr.join('');
+}
 
 module.exports = morse;
