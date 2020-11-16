d9eb1979d538e73dd79a53d2f872aa0f5d724fcd
Anthony Benarivo <benarivo66@gmail.com>
2020-11-16 13:07:53 +0100

    remove duplicates from any obj

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,39 +1,80 @@
-function isolateDuplicates(text) {
-    if (typeof text !== "string") {
-        return "Please enter a valid string"
-    }
-    let arr = text.split("");
-    let newArr = [...new Set(arr)];
-    
-    let newArr1 = newArr.map(txt => txt.repeat(count(txt))); 
-    let newArr2 = [];
+// function isolateDuplicates(text) {
+// if (typeof text !== "string") {
+//     return "Please enter a valid string"
+// }
+//     let arr = text.split("");
+//     let newArr = [...new Set(arr)];
 
-    for (let i = 0; i < newArr1.length; i++) {
-        if (newArr1[i].length > 2) {
-            newArr2.push(newArr1[i].substring(0, 2) + "[" + newArr1[i].substring(2, newArr1[i].length) + "]")
-        }else {
-            newArr2.push(newArr1[i]);
-        }
-}
-    function count(chr) {
-        let str = text;
-        let strl = str.length;
-        for (i = l = 0; i < strl; i++) {
-            if (str[i] === chr) {
-                l++;
+//     let newArr1 = newArr.map(txt => txt.repeat(count(txt))); 
+//     let newArr2 = [];
+
+//     for (let i = 0; i < newArr1.length; i++) {
+//         if (newArr1[i].length > 2) {
+//             newArr2.push(newArr1[i].substring(0, 2) + "[" + newArr1[i].substring(2, newArr1[i].length) + "]")
+//         }else {
+//             newArr2.push(newArr1[i]);
+//         }
+// }
+//     function count(chr) {
+//         let str = text;
+//         let strl = str.length;
+//         for (i = l = 0; i < strl; i++) {
+//             if (str[i] === chr) {
+//                 l++;
+//             }
+//         }
+
+//         return l;
+//     }
+//      let newStr = newArr2.join('')
+//      let counts =  newStr.match(/]/g).length;
+
+//     let newArr3 = [];
+//     newArr3.push(newStr);
+//     newArr3.push(counts);
+//     return newArr3;
+
+//      }
+
+
+function isolateDuplicates(text) {
+    if (typeof text !== "string") throw Error("Please enter a valid string")
+    let newText = text[0];
+    let prev = text[0];
+    let totalCount = 0;
+    let count = 1;
+    for (let l of text.slice(1)) {
+        if (l.toLowerCase() !== prev.toLowerCase()) {
+
+
+            //   newText += count < 3 ? "l" : `]${l}`;
+            if (count < 3) {
+                newText += l;
+            } else {
+                newText += `]${l}`;
+            }
+
+            //   } if (count < 2) {
+
+            count = 1;
+        } else {
+            count += 1;
+            if (count === 3) {
+                newText += `[${l}`;
+                totalCount += 1;
+            }
+
+            else {
+                newText += l
             }
         }
-        
-        return l;
+        prev = l;
     }
-     let newStr = newArr2.join('')
-     let counts =  newStr.match(/]/g).length;
 
-    let newArr3 = [];
-    newArr3.push(newStr);
-    newArr3.push(counts);
-    return newArr3;
-    
-     }
+    if (count > 2) newText += "]"
+
+    return [newText, totalCount]
+}
+
 
 module.exports = isolateDuplicates;
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,19 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    let keyA = Object.keys(obj);
+    for (let i = keyA.length-1; i >= 0; i--) {
+        let item = obj[keyA[i]];
+        let checkArr = [];
+        for (let j = 0; j < item.length; j++) {
+            if(checkArr.includes(item[j]) === true) {
+                item.splice(j, 1);
+                checkArr.push(item.join(","))
+            }else {
+                checkArr.push(item.join(","));
+            }
+        }
+    }
+    return obj;
+
+}
 
 module.exports = removeDuplicates;
