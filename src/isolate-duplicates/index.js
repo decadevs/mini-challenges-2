656659c3885697327b146e2a4dc3b48d6d283f68
ObioraM <obioramaduakor@gmail.com>
2021-02-26 15:12:51 +0100

    Solve isolate-duplicates

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,39 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    
+    if (typeof(text)!= 'string') throw 'Please enter a valid string';
+    
+    let countDuplicates = 0;
+    const trackDuplicates = {};
+    const arr = [];
+
+    for (let i=0; i < text.length; i++) {
+        if (i != 0) {
+            if (text[i-1].toLowerCase() != text[i].toLowerCase()) {
+                if (trackDuplicates[text[i-1].toLowerCase()]['alphaCount']>2) {
+                    arr.push("]");
+                }
+                trackDuplicates[text[i-1].toLowerCase()]['alphaCount'] = 0;
+            }
+        }
+
+        if (!trackDuplicates.hasOwnProperty(text[i].toLowerCase())) {
+            trackDuplicates[text[i].toLowerCase()] = {'alphaCount': 1};
+            arr.push(text[i]);
+        } else {
+            if (trackDuplicates[text[i].toLowerCase()]['alphaCount'] == 2) {
+                countDuplicates += 1;
+                arr.push('[');
+            }
+            arr.push(text[i]);
+            trackDuplicates[text[i].toLowerCase()]['alphaCount'] += 1;
+        }
+    }
+
+    if (trackDuplicates[text[text.length-1].toLowerCase()]['alphaCount'] > 2 ) {
+        arr.push(']');
+    }
+
+    return [arr.join(""), countDuplicates];
+}
 
 module.exports = isolateDuplicates;
