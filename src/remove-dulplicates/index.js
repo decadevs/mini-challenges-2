4f8761239474ab8571e7e531cb192d1184d6a4e4
ObioraM <obioramaduakor@gmail.com>
2021-03-02 15:11:18 +0100

    Complete remove-duplicates

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,22 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    const keysArray = Object.keys(obj);
+    keysArray.sort((a,b)=> a-b);
+    const objBlocker = {};
+
+    for (let i = keysArray.length -1; i>=0; i--) {
+        let arr = [];
+
+        for (let j = 0; j < obj[keysArray[i]].length; j++){
+            if (objBlocker.hasOwnProperty(obj[keysArray[i]][j])) {
+                continue;
+            }
+            arr.push(obj[keysArray[i]][j]);
+            objBlocker[obj[keysArray[i]][j]] = 0;
+        }
+        obj[keysArray[i]] = arr;
+    }
+    return obj;
+}
+
 
 module.exports = removeDuplicates;
