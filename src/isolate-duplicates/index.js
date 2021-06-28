4762b4085d3aacf799ec588f8a955299e68f62f8
Ebuka <bukasin1@gmail.com>
2021-06-28 11:51:53 +0100

    commits

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,37 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    
+    if( typeof(text) !== "string"){ throw new Error('Please enter a valid string') }
+        let lowerText = text.toLowerCase()
+        let check = [], duplicates = [], str = '', count = 0, array = []
+        for(let i=0; i< text.length; i++){
+            if(lowerText[i+1]===lowerText[i]){
+                if(check.length<2){
+                    check.push(text[i])
+                }else{duplicates.push(text[i])}
+            }else{
+                if(check.length<2){
+                    check.push(text[i])
+                }else{duplicates.push(text[i])}
+                for(items of check){
+                    str += items
+                }
+                if(duplicates.length > 0){
+                    count += 1
+                    str += '['
+                    for(items of duplicates){
+                        str += items
+                    }
+                    str += ']'
+                }
+                check = []
+                duplicates = []
+            }
+        }
+        array.push(str,count)
+        return array   
+    
 
+}
+
+//console.log(isolateDuplicates("How are you"))
 module.exports = isolateDuplicates;
