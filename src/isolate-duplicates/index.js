af88713c584b5a42ad7e826a623a08a87a8e6a96
Dec <dec@Decagons-MacBook-Air.local>
2022-06-20 11:29:47 +0100

    Solutions to mini-challenges-2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,41 @@
-function isolateDuplicates(text) {}
+/**
+ * @param {string} text The string of brackets.
+ * @returns {[string, number]} The duplicates in brackets and total duplicates. 
+ */
+
+ function isolateDuplicates(text) {
+    
+    if(typeof(text) !== "string"){
+        throw "Please enter a valid string";
+    }
+
+    let countDuplicates = 0;
+    let totalDuplicates = 0;
+    const duplicatesArray = []; 
+
+    //loop through text string and push accordingly
+    for(let i=0; i<text.length ; i++){
+        duplicatesArray.push(text[i]);
+        if(((text[i+1] !== undefined) && text[i].toLowerCase() === text[i+1].toLowerCase())){
+            
+            if( countDuplicates === 1){
+                duplicatesArray.push('[');
+            }
+            countDuplicates++;
+
+        }
+        else if(((text[i+1] !== undefined) && text[i].toLowerCase() !== text[i+1].toLowerCase()) || (i === text.length - 1)){//check if end of string
+           
+            if(countDuplicates > 1){
+                duplicatesArray.push(']');  
+                totalDuplicates ++;
+            }
+            countDuplicates = 0;
+                      
+        }
+    }
+    return [duplicatesArray.join(""), totalDuplicates];
+    
+}
 
 module.exports = isolateDuplicates;
--- src/morse/index.js
+++ src/morse/index.js
@@ -1,4 +1,10 @@
-const MORSE_CODE = {
+/**
+ * @param {string} text The morse code.
+ * @returns {string[]} The decoded string.
+ */
+
+
+ const MORSE_CODE = {
   "-.-.--": "!",
   ".-..-.": '"',
   "...-..-": "$",
@@ -58,6 +64,37 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text) {
+
+ 
+    if(typeof(text) !== "string"){
+        throw ('Please provide a morse string');
+    }
+    if(text == ""){
+        return text;
+    }
+    //check if string includes morse code characters
+    if(text.indexOf('.') === -1 && text.indexOf('-')=== -1){ 
+        throw ("Invalid morse code");
+    }
+
+    const morseArr = text.split(' ');
+    const decodedArr = [];
+    let countSpaces = 0;
+
+    //decode morse code and keep track of whitespaces 
+    for(let i=0; i < morseArr.length; i++){       
+          decodedArr.push(MORSE_CODE[morseArr[i]])
+          if(morseArr[i] === ''){
+              countSpaces++;
+          }
+          if(countSpaces === 2 && text.length > 1){
+              decodedArr.push(' ');
+              countSpaces = 0;
+          }   
+    }
+    return decodedArr.join('').trim();
+
+} 
 
 module.exports = morse;
