0a660b0254194ea6da832c8e3249a24d3c7c3c9b
christopher-moses <christmoses1999@icloud.com>
2022-06-21 17:50:45 +0100

    second assignment

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,26 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+
+  const keyHolder = Object.keys(obj);
+  keyHolder
+  const duplicated = {};
+  let arrayPseudo = [];
+
+  keyHolder.reverse().forEach((key) => {
+
+      obj[key] = [...new Set(obj[key])];
+      //
+      obj[key].forEach((char) => {
+          if (!duplicated[char]) {
+              duplicated[char] = true;
+              arrayPseudo.push(char);
+          }
+      });
+      obj[key] = arrayPseudo;
+      arrayPseudo = [];
+  });
+  return obj;
+
+}
+ 
 
 module.exports = removeDuplicates;
