8d2c5a236a91d2382bb76db8c5f0038a8b925b5c
Dec <dec@Richie.local>
2022-06-21 16:05:35 +0100

    challenge 2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,33 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+
+  if(arguments.length <1 || typeof(text) != "string"){
+    throw "Please enter a valid string"
+  }
+  let Arr= []
+  let dupliCount= 0;
+  let totalCount= 0
+  text= text.split('');
+  for(let i=0; i < text.length; i++){
+    Arr.push(text[i])
+    if(text[i+1] != undefined && text[i].toLowerCase() == text[i+1].toLowerCase()){
+      dupliCount++
+      if(dupliCount==2){
+        Arr.push('[')
+        totalCount++
+      }
+
+    }else if(text[i+1] != undefined && text[i].toLowerCase() !== text[i+1].toLowerCase() || i == text.length-1){
+      if(dupliCount > 1){
+        Arr.push(']')
+      }
+      dupliCount= 0
+    }
+  }
+
+  return [Arr.join(''), totalCount]
+
+
+
+}
 
 module.exports = isolateDuplicates;
