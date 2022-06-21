0f4b50ac217db154234ced52569805adceef8aa3
beastlywallace <akonakpevwe@gmail.com>
2022-06-21 15:15:23 +0100

    Mini-Challenges2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,50 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+  let similarLetters = [];
+  let arrayOfDuplicates = [];
+
+    if (typeof text !== "string") {
+      throw new Error("Please enter a valid string");
+    } else {
+
+        let finalValues = [];
+        let totalDuplicates = 0;
+
+      for (let i = 0; i < text.length; i++) {
+        let oneIndexBack = text[i - 1 ]
+        let anIndexForward = text[i + 1]
+        if (String(text[i]).toUpperCase() == String(anIndexForward).toUpperCase()
+        || String(text[i]).toUpperCase() == String(oneIndexBack).toUpperCase()
+        ) {
+          if (arrayOfDuplicates.length < 2) {
+            arrayOfDuplicates.push(text[i]);
+            if (arrayOfDuplicates.length == 2)
+            {
+              finalValues += arrayOfDuplicates[0];
+              finalValues += arrayOfDuplicates[1];
+           }
+        } else if (String(text[i]).toUpperCase() != String (oneIndexBack).toUpperCase() &&
+          String(text[i]).toUpperCase() ==  String(anIndexForward).toUpperCase())
+        {
+          arrayOfDuplicates = [];
+          arrayOfDuplicates.push(text[i]);
+        } else if (String(text[i]).toUpperCase() == String(oneIndexBack).toUpperCase() &&
+        String(text[i]).toUpperCase() != String(anIndexForward).toUpperCase())
+        {
+          similarLetters += text[i];
+          finalValues += "[" + similarLetters + "]";
+          totalDuplicates++;
+          similarLetters= "";
+        } else {
+          similarLetters += text[i];
+        }
+        }else{
+          finalValues += text[i];
+        }
+    }
+   return [finalValues, totalDuplicates];
+  }
+}
+
+
 
 module.exports = isolateDuplicates;
--- src/morse/test.js
+++ src/morse/test.js
@@ -1,4 +1,4 @@
-const morseToText = require(".");
+const morseToText = require("./index.js");
 
 describe("Morse assumptions", () => {
   test("it returns an empty string untouched", () => {
