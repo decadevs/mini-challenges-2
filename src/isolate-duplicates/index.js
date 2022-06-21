cd1596abe717810291df50889ee62b2aa247351a
Franklin Osigwe <franklindutch1@gmail.com>
2022-06-21 15:55:28 +0100

    commit

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,34 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    if(typeof text !== "string") throw ("The String is empty")
+      let vals= [];            //[a a]
+      let counter = 0;
+      let duplicateCounter = 0;//0+1+1
+      let newText= text.toLowerCase();                
+      for(let i = 0; i< newText.length; i++)
+      {
+        vals.push(text[i]);  
+        if(newText[i] == newText[i+1])
+        {
+          if(counter === 1) 
+          {
+            vals.push("[")
+            duplicateCounter++
+          }
+          counter++;
+        }else if(newText[i] !== newText[i+1] && counter >1|| i == newText.length -1 && counter > 1)
+        {
+          counter = 0;
+          vals.push("]");
+        }
+        else{
+          counter =0;
+      }
+    }
+    return [vals.join(""), duplicateCounter]
+  }
+console.log(isolateDuplicates("dfghrtrytfkhgggggddccccjjh"))
+  
+  module.exports = isolateDuplicates;
 
-module.exports = isolateDuplicates;
+
+
