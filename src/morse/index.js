e8d692dc8082fd1cd976855eb36fe504ae2212c3
New User <laptop@News-MacBook-Pro.local>
2022-06-21 05:00:12 -0700

    mini-challenge 2

--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,21 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text) {
+  if (text === '') return ''
+  if (typeof text !== 'string') throw new Error('Please provide a morse string')
+  const input = text.trim()
+  const words = input.split(/\s{3,}/)
+
+  let result = ''
+  for (let i =0; i < words.length; i++){
+    let word = words[i]
+    let characters = word.split('')
+    for (let j = 0; j < characters.length; j++){
+        let clear = character [j]
+    }
+  }
+  return result.trim()
+}
 
 module.exports = morse;
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,29 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+  for (let objKey in obj) {
+
+    obj[objKey] = [...new Set(obj[objKey])]
+   }
+   let tempObj = obj
+
+   for (let objKey in obj) {
+    obj[objKey].forEach((element) => {
+
+
+      for(let objKey2 in tempObj){
+        tempObj[objKey2].forEach((element2) => {
+
+          if(element === element2 && objKey !=
+            objKey2){
+            tempObj[objKey] = tempObj[objKey].filter(
+              (value) => value !== element)
+          }
+        });
+      }
+
+    });
+   }
+
+   return tempObj
+}
 
 module.exports = removeDuplicates;
