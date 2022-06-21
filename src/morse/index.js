e4cc054e4942787ff34a9157821fbc4840b9a959
Dec <dec@GentleStorms-Air.local>
2022-06-21 15:33:18 +0100

    morse code solution

--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,33 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text) {
+  const regex = new RegExp(/^[-. ]/)
+  
+  if(text === ""){
+    return "";
+  }
+   else if(regex.test(text) === false){
+    throw new Error ('Please provide a morse string')
+  } else {
+    /*
+    1. use 3 spaces to seperate words with space
+    2. then use 1 space to separate words without space
+    3. then loop through the morse dictionary with morse codes for converstion
+    4. use join to convert from array to spring also inserting right spacing
+    5. trim removes spaces from the edges
+    */
 
+    return text.split("   ")
+                .map(text => text
+                .split(" ") 
+                .map(character => MORSE_CODE[character]) 
+                .join('')
+        ).join(' ') 
+         .trim()
+  }
+}
+
+
+morse("");
 module.exports = morse;
