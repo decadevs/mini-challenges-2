6065eafc86676f4efff55bfb8e8aed76336c9be1
Benji4C <thegreatben27@gmail.com>
2022-06-21 16:32:42 +0100

    Challenge 2 soultion

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,41 @@
-function isolateDuplicates(text) {}
 
-module.exports = isolateDuplicates;
+  function isolateDuplicates(text) {
+    if (typeof(text) !== "string") throw new Error("Please enter a valid string");
+
+    let caseText = text.toUpperCase();
+    let ref = caseText[0], arrayOfDups = [];
+    let tracker = 0, ifNoOpening = false, duplicates = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        //checking if 2 duplicates already exists, and opening with '[' if so
+      if (tracker === 2 && caseText[i] === arrayOfDups[arrayOfDups.length - 1].toLocaleUpperCase()) {
+        arrayOfDups.push('[');
+        ifNoOpening = true;
+      }
+      // changing the ref and subsequently closing with ']' if open
+      if (ref!== caseText[i]) {
+        ref = caseText[i];
+        tracker = 0;
+        if (ifNoOpening) {
+          arrayOfDups.push(']');
+          ifNoOpening = false;
+          duplicates++;
+        }
+      }
+      //pushing individual alphabets
+      arrayOfDups.push(text[i]);
+      tracker++;
+      //checking to close it off if its open and the last letter
+      if (i === text.length - 1 && ifNoOpening) {
+        arrayOfDups.push(']');
+        duplicates++;
+      }
+    }
+    return [...arrayOfDups.join('').split(), duplicates]
+  }
+  module.exports = isolateDuplicates;
+
+
+
+
+
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,35 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+  const uniqueVal = [];
+  const elemFound = [];
+  const finalAns = {};
+
+  const keys = Object.keys(obj)
+      .map(Number)
+      .sort((a, b) => b - a);
+
+  while (uniqueVal.length < keys.length) {
+      uniqueVal.push([])
+  }
+
+  let numCount = 0;
+  for (let arr of keys) {
+      const array = obj[arr];
+
+      array.forEach(e => {
+          if (!elemFound.includes(e)) {
+              uniqueVal[numCount].push(e);
+              elemFound.push(e)
+          }
+      })
+
+      numCount+= 1
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+      finalAns[keys[i]] = uniqueVal[i]
+  }
+  return finalAns
+}
+
 
 module.exports = removeDuplicates;
