94fc094d8c29dc08306ac2dda7b5eec66e28a12c
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-06-21 15:36:36 +0100

    Completed Mini-challenge 2

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,37 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+	let arrKey = Object.keys(obj).sort((a, b) => b - a);
+	const seen = [];
+	const returnObj = {};
+
+	for (let i = 0; i < arrKey.length; i++) {
+		for (let j = 0; j < obj[arrKey[i]].length; j++) {
+
+			if (!returnObj.hasOwnProperty(arrKey[i])) {
+				returnObj[arrKey[i]] = [];
+			}
+			if (!seen.includes(obj[arrKey[i]][j])) {
+				// console.log(returnObj[arrKey[i]].filter(e=> e))
+				returnObj[arrKey[i]][j] = obj[arrKey[i]][j];
+				seen.push(obj[arrKey[i]][j]);
+			}
+		}
+	}
+
+	//this filters empty array item
+	for (let a in returnObj) {
+		let newArr = returnObj[a].filter(a => a)
+
+		returnObj[a] = newArr
+	}
+
+	// console.log(returnObj)
+	return returnObj
+}
+
+// removeDuplicates({
+// 	"1": ["C", "F", "G"],
+// 	"2": ["A", "B", "C"],
+// 	"3": ["A", "B", "D"],
+// });
 
 module.exports = removeDuplicates;
