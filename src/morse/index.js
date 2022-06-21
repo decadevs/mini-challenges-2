df9ae000184f5f066cf5e5e81ce2b0b2bceb16f4
User <user@Users-MacBook-Air.local>
2022-06-21 16:00:35 +0100

    solution

--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,29 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text){
+  //split text and check for length
+  let newLen = text.length;
+  text = text.trim();
+  text = text.split(" ");
+  let output = "";
+  if (typeof text !== String){
+    throw ("please provide a morse string")
+  }
+
+  // loop through the newLen and check for corresponding values in object.
+  for(let i = 0; i < newLen; i++){
+      let n = text[i], n1 = text[i + 1];
+      if(MORSE_CODE[n]){
+          output += MORSE_CODE[n];
+      }
+      //a command to tell program that two spaces after separator space(for character)is a word.
+      else if(n == "" && n1 == ""){
+          output += " ";
+      }
+  }
+  return output.trim();
+}
+
 
 module.exports = morse;
