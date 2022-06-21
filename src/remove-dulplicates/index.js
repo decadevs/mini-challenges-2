326b92d2151de1acb098b18115e7f7be5f9037e5
parishub <greatokhawere51@gmail.com>
2022-06-21 13:51:33 +0100

    mini-challenge-2

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,39 @@
-function removeDuplicates(obj) {}
+
+ const obj = {
+  "1": ["C", "F", "G"],
+  "2": ["A", "B", "C"],
+  "3": ["A", "B", "D"],
+};
+
+function removeDuplicates(obj) {
+
+  const sortedKeys = Object.keys(obj);
+  const keys = sortedKeys.reverse();
+
+  const duplicates = [];
+  const noDuplicates = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    for (let j = 0; j < obj[keys[i]].length; j++) {
+      if (!duplicates.includes(obj[keys[i]][j])) {
+        if (noDuplicates[keys[i]] === undefined) {
+          noDuplicates[keys[i]] = [];
+        }
+        noDuplicates[keys[i]][j] = obj[keys[i]][j];
+        duplicates.push(obj[keys[i]][j]);
+      }
+    }
+
+    if (noDuplicates[keys[i]] === undefined) {
+    } else {
+      noDuplicates[keys[i]] = noDuplicates[keys[i]].filter(
+        (el) => el !== undefined
+      );
+    }
+  }
+
+  return noDuplicates;
+   }
 
 module.exports = removeDuplicates;
+
