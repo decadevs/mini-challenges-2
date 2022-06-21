47c4acf4c60b228aa19b6a8677bf912fc421cb10
Imeh Usoro <imeusoro@rocketmail.com>
2022-06-21 15:01:04 +0100

    all test passed

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,33 +1,61 @@
-function removeDuplicates(obj) {
-  const sortedKeys = Object.keys(obj);
-  const keys = sortedKeys.reverse();
+// function removeDuplicates(obj) {
+//   const sortedKeys = Object.keys(obj);
+//   const keys = sortedKeys.reverse();
 
-  const duplicates = [];
-  const noDuplicates = {};
+//   const duplicates = [];
+//   const noDuplicates = {};
+
+//   for (let i = 0; i < keys.length; i++) {
+//     for (let j = 0; j < obj[keys[i]].length; j++) {
+//       if (!duplicates.includes(obj[keys[i]][j])) {
+//         if (noDuplicates[keys[i]] === undefined) {
+//           noDuplicates[keys[i]] = [];
+//         }
+//         noDuplicates[keys[i]][j] = obj[keys[i]][j];
+//         duplicates.push(obj[keys[i]][j]);
+//       }
+//     }
+
+//     if (noDuplicates[keys[i]] === undefined) {
+//     } else {
+//       noDuplicates[keys[i]] = noDuplicates[keys[i]].filter(
+//         (el) => el !== undefined
+//       );
+//     }
+//   }
+
+//   return noDuplicates;
+// }
+function removeDuplicates(obj) {
+  const unique = [];
+  const allSeen = [];
+  const output = {};
+
+  const keys = Object.keys(obj)
+    .map(Number)
+    .sort((a, b) => b - a);
+
+  while (unique.length < keys.length) {
+    unique.push([]);
+  }
+
+  let latter = 0;
+  for (let arr of keys) {
+    const array = obj[arr];
+
+    array.forEach((e) => {
+      if (!allSeen.includes(e)) {
+        unique[latter].push(e);
+        allSeen.push(e);
+      }
+    });
+
+    latter += 1;
+  }
 
   for (let i = 0; i < keys.length; i++) {
-    for (let j = 0; j < obj[keys[i]].length; j++) {
-      if (!duplicates.includes(obj[keys[i]][j])) {
-        if (noDuplicates[keys[i]] === undefined) {
-          noDuplicates[keys[i]] = [];
-        }
-        noDuplicates[keys[i]][j] = obj[keys[i]][j];
-        duplicates.push(obj[keys[i]][j]);
-      }
-    }
-    console.log(noDuplicates);
-    console.log(keys[i]);
-
-    if (noDuplicates[keys[i]] === undefined) {
-    } else {
-      noDuplicates[keys[i]] = noDuplicates[keys[i]].filter(
-        (el) => el !== undefined
-      );
-    }
+    output[keys[i]] = unique[i];
   }
-  console.log(noDuplicates);
-
-  return noDuplicates;
+  return output;
 }
-
 module.exports = removeDuplicates;
