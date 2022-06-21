694f53eb237ce63e221b19708493a73834e3396f
Dec <dec@Excel.local>
2022-06-21 15:52:19 +0100

    Solution to challenge 2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,36 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+  if (arguments.length < 1 || typeof text != "string") {
+    throw "Please enter a valid string";
+  }
+  let newcontainer = [];
+  let Duplicates = 0;
+  let totalCount = 0;
+  text = text.split("");
+
+  for (let i = 0; i < text.length; i++) {
+    newcontainer.push(text[i]);
+    if (
+      text[i + 1] != undefined &&
+      text[i].toLowerCase() == text[i + 1].toLowerCase()
+    ) {
+     Duplicates++;
+      if ( Duplicates == 2) {
+        newcontainer.push("[");
+        totalCount++;
+      }
+    } else if (
+      (text[i + 1] != undefined &&
+        text[i].toLowerCase() !== text[i + 1].toLowerCase()) ||
+      i == text.length - 1
+    ) {
+      if ( Duplicates > 1) {
+        newcontainer.push("]");
+      }
+     Duplicates = 0;
+    }
+  }
+
+  return [newcontainer.join(""), totalCount];
+}
 
 module.exports = isolateDuplicates;
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,23 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
 
+  const mainKey = Object.keys(obj);
+  mainKey.sort((a, b) => a - b);
+  const objContainer = {};
+
+  for (let i = mainKey.length - 1; i >= 0; i--) {
+    let arr = [];
+
+    for (let j = 0; j < obj[mainKey[i]].length; j++) {
+
+      if (objContainer.hasOwnProperty(obj[mainKey[i]][j])) {
+        continue;
+      }
+      arr.push(obj[mainKey[i]][j]);
+      objContainer[obj[mainKey[i]][j]] = 0;
+    }
+    obj[mainKey[i]] = arr;
+  }
+  return obj;
+
+}
 module.exports = removeDuplicates;
