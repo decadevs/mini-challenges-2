61d06a52d90e1e50092285ef2c3485acde168a4f
anthonyponplex <mayakianthony@yahoo.com>
2022-06-21 15:52:02 +0100

    challenge 2 solution

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,32 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+  if(arguments.length < 1 || typeof(text) != "string"){
+    throw "please enter a valid string"
+  }
 
+  let newArr = []
+  let multiCount = 0;
+  let universalCount = 0;
+
+  text = text.split("");
+  for(let i = 0; i < text.length; i++){
+    newArr.push(text[i])
+    if(text[i+1] != undefined && text[i].toLowerCase() == text[i+1].toLowerCase()){
+      multiCount++
+      if(multiCount == 2){
+        newArr.push("[")
+        universalCount++
+      }
+
+    }else if (text[i+1] != undefined && text[i].toLowerCase() !== text[i+1].toLowerCase() || i == text.length - 1){
+      if(multiCount > 1 ){
+        newArr.push("]")
+      }
+      multiCount = 0
+    }
+  }
+
+  return[newArr.join(""),universalCount]
+}
+
+console.log(isolateDuplicates('qqwhgehjelidgddskjdhshslisddzhjsaassss'))
 module.exports = isolateDuplicates;
