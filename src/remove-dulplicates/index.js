99fa7ede68bb4da21e7ce7db140ad1f62bdff079
Franklin Osigwe <franklindutch1@gmail.com>
2022-06-21 15:55:54 +0100

    commit

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,25 @@
-function removeDuplicates(obj) {}
-
-module.exports = removeDuplicates;
+function removeDuplicates(obj) {
+    let newArr = {};
+      for (i in obj) {
+        newArr[i] = Array.from(new Set(obj[i])); 
+      }
+  
+      let objKeys = Object.keys(newArr);
+      let sortedObjKeys = objKeys.sort((a, b) => a - b).reverse();
+      let controlArray = [];
+      const outputObj = {};
+  
+      sortedObjKeys.forEach((key) => {
+          let array = [];
+          newArr[key].forEach((value) => {
+              if (controlArray.indexOf(value) === -1) {
+                  array.push(value);
+              }
+              controlArray.push(value);
+          });
+          outputObj[key] = array;
+      });
+      return outputObj;
+  }
+  
+  module.exports = removeDuplicates;
