16fde5f30e8d710013893d490dca0c68b15b7b15
Dec <dec@Oluwaseun.local>
2022-06-21 09:30:44 +0100

    Algo-2 Solutions

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,50 @@
-function isolateDuplicates(text) {}
+
+function isolateDuplicates(text) {
+
+    if(!(typeof text === 'string')){
+        throw 'Please enter a valid string' // Check input type and throw error if not string
+    }
+    else{
+        let duplicates = ''; // Declare duplicate of an empty string
+        let duplicatesCount = 0; // Initianlize duplicates count
+        let acceptableDuplicate = `${text[0]}${text[1]}` // This defines the first two char of text
+
+        acceptableDuplicate
+
+        var textArray = text.split('') // Convert text to array
+        
+        for (let i = 2; i < textArray.length; i++){ // Start iterator after acceptable duplicates
+
+            let lastAcceptableDuplicateCharIndex = acceptableDuplicate.length - 1; // 1
+            let firstAcceptableDuplicateCharIndex = acceptableDuplicate.length - 2 // 0
+
+            if(textArray[i].toLowerCase() === acceptableDuplicate.charAt(lastAcceptableDuplicateCharIndex)
+            .toLowerCase() && textArray[i].toLowerCase() === acceptableDuplicate.charAt(firstAcceptableDuplicateCharIndex)
+            .toLowerCase()){ // check if next char is same as acceptable duplicates
+                duplicates += textArray[i] // Append such occurences to duplicate string
+             
+               
+                if((textArray.length - 1) > i && textArray[i]
+                .toLowerCase() !== textArray[i + 1].toLowerCase()){ // If text array still has values and two consecutive element isn't the same
+                    
+                    acceptableDuplicate += `[${duplicates}]` // Box existing duplicates in [ ] and append to first to acceptable duplicates
+                    duplicatesCount++ // Add 1 to duplicates count
+                    duplicates = '' // Reset duplicates holder to be empty to avoid appending old duplicates to next duplicates
+                }
+            }else{
+                    acceptableDuplicate += textArray[i] // append char if its not the same as acceptable duplicates
+                }
+        }
+
+        if(duplicates.length > 0){
+            acceptableDuplicate += `[${duplicates}]`;
+            duplicatesCount++
+        }
+        
+        return [acceptableDuplicate, duplicatesCount]
+ 
+    }
+    
+}
 
 module.exports = isolateDuplicates;
--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,84 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+// function is used to return a count of 1 when two or more consecutive spaces are encountered
+function spaceOccurencesCounter(string, word) {
+  let count = 0;
+  for(let i = 0; i < string.length; i++){
+    if(string[i] === word && string[i + 1] === word){
+      count ++
+    }
+  }
+  return count
+}
+
+// Function decrypts Morse code to readable text
+function decryptedMorseCode(encryptedText){
+  let decryptedTextArr = [];
+  let encryptedTextLength = encryptedText.length
+
+  for(let i = 0; i < encryptedTextLength; i++){
+    decryptedTextArr.push(MORSE_CODE[encryptedText[i]])
+  }
+  return decryptedTextArr
+}
+
+
+function morse(text) {
+
+  if(text === ''){ // checks for empty string
+    return ''
+  }else if (typeof(text) != 'string'){ // checks for non string characters
+    throw 'Please provide a morse string'
+  }
+  else{
+    
+    if(spaceOccurencesCounter(text, ' ') > 0 ){ //If two or more consecutive spaces is seen
+      textWith2Spaces = text.toString().trim() // Trim text
+
+      let stringifyArr = textWith2Spaces.split(' ')
+   
+      let result = decryptedMorseCode(stringifyArr).map(value => { //Map through readable text and replace undefined with empty string
+        if(value === undefined){
+          value = ''
+        }
+        return value
+      })
+
+      let morseMessage = []
+      for (k = 0; k < result.length; k++){
+        
+        if(!(result[k] === result[k + 1] && result[k] === '')){
+          morseMessage.push(result[k])
+          if(result[k] === ''){
+            morseMessage.push(' ') // Convert empty string to single space
+          }
+        }
+      }
+  
+      return morseMessage.join('') //return morse message
+
+    }else{ // if single space is seen
+      text = text.toString() // Process text without trimming
+      
+      let textWith3Spaces = text.split(' ')
+      
+      let newResult = decryptedMorseCode(textWith3Spaces).map(value => {
+        if(value === undefined){
+          value = ''
+        }
+        
+        return value
+      })
+      
+      let messageOutput = newResult.join('').trim()
+
+      return messageOutput
+    }
+  }
+    
+}
 
 module.exports = morse;
+
+
