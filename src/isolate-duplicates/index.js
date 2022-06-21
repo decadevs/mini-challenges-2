b2f65bfce2b4c1c51fc19c26d515c94177487c13
dannack <felixtemikotan@yahoo.com>
2022-06-21 09:03:41 +0100

    Updated challenge-2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,30 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    let strNew = "";
+    let mult = {};
+    let counter = 0;
 
+    if (typeof text !== "string") {
+        throw new Error("Please enter a valid strings")
+    } else {
+        const str = text.toUpperCase();
+
+        for (let i = 0; i < text.length; i++) {
+            if (str[i].repeat(2) === str[i - 2] + str[i - 1]) {
+
+                if (!mult[str[i]]) {
+                    mult[str[i]] = text[i];
+                } else {
+                    mult[str[i]] += text[i]
+                }
+
+                if (str[i] !== str[i + 1]) {
+                    strNew += "[" + mult[str[i]] + "]";
+                    counter += 1;
+                    mult = {};
+                }
+            } else { strNew += text[i]; }
+        }
+        return [strNew, counter]
+    }
+}
 module.exports = isolateDuplicates;
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,20 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    const keys = Object.keys(obj);
+    const isDuplicate = {};
+    let dummy = [];
 
+    keys.reverse().forEach((key) => {
+
+        obj[key] = [...new Set(obj[key])];
+        obj[key].forEach((char) => {
+            if (!isDuplicate[char]) {
+                isDuplicate[char] = true;
+                dummy.push(char);
+            }
+        });
+        obj[key] = dummy;
+        dummy = [];
+    });
+    return obj;
+}
 module.exports = removeDuplicates;
