2a917c6a53b5698aa8669f387fc2fd62788e0eff
Dec <dec@GentleStorms-Air.local>
2022-06-30 12:29:01 +0100

    little house keeping

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,23 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+   const keyOfArr = Object.keys(obj);
+    keyOfArr.sort((a, b) => a - b);
+    const blocker = [];
+
+    for (let j = keyOfArr.length - 1; j >= 0; j--) {
+      let arr = [];
+
+      for (let k = 0; k < obj[keyOfArr[j]].length; k++) {
+        if (blocker.hasOwnProperty(obj[keyOfArr[j]][k])) {
+          continue;
+        }
+        arr.push(obj[keyOfArr[j]][k]);
+        blocker[obj[keyOfArr[j]][k]] = 0;
+      }
+      obj[keyOfArr[j]] = arr;
+    }
+    return obj;
+  }
+
+
 
 module.exports = removeDuplicates;
