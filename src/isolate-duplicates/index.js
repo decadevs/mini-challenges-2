a2be4ac285de503794a88200276c3bce73df20a7
skipper <skippergoroye>
2022-09-12 16:44:13 +0100

    challenge 2 solution

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,23 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    if (typeof text !== "string") return "Please enter a valid string";
+    let arr = [];
+    let str = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i - 1] === text[i]) {
+        str += text[i];
+      } else {
+        arr.push(str);
+        str = text[i];
+      }
+      if (i === text.length - 1) {
+        arr.push(str);
+      }
+    }
+    return arr.map(v =>v.length > 2 ? v.slice(0, 2) + "[" + v.slice(2) + "]" : v).join``;
+}
+
+
+console.log(isolateDuplicates(`"aaaabbcdefffffffg"`))
+
 
 module.exports = isolateDuplicates;
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,19 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    var keys = Object.keys(obj)
+    keys.sort().reverse()
+    seenBefore = new Set()
+    answer = {}
+    for (var key of keys) { 
+        for (var value of obj[key]) {
+            if (!seenBefore.has(value)) {
+                answer[key] = [...(answer[key] || []), value]
+                seenBefore.add(value)
+            }
+        }
+    }
+    return answer
+}
+
+
 
 module.exports = removeDuplicates;
