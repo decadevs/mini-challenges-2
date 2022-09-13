9721d2b64d1e5cdffcd64cf3300dfe02e30a8b0a
Oluwaseyi <oluwaseyi.makinde@decagon.dev>
2022-09-13 07:38:32 -0700

    added features

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,18 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    let keys = Object.keys(obj).sort((a, b) => Number(b) - Number(a));
+    for (let key in obj) {
+      let newValues = new Set(obj[key]);
+      obj[key] = [...newValues];
+    }
+  
+    let myValues = obj[keys[0]];
+  
+    keys.slice(1).forEach (function (key) {
+      obj[key] = obj[key].filter(element => !myValues.includes(element));
+      myValues = myValues.concat(obj[key]);
+    });
+  
+    return obj;
+}
 
 module.exports = removeDuplicates;
