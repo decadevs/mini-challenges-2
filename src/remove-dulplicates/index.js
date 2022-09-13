2d2bc0c50637deeaf0b4f89c9348b32832145f8e
glorylaw <100371169+glorylaw@users.noreply.github.com>
2022-09-13 16:19:00 +0100

    Squashed commit of the following:
    commit 5490ed314594e19a781c6d9d1fd0141285869d98
    Author: glorylaw <100371169+glorylaw@users.noreply.github.com>
    Date:   Tue Sep 13 16:17:45 2022 +0100
    
        submission

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,13 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    let letters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    let result= []
+    let keyNumber=key.parseInt()
+    for(let i=0;i<obj[0].length;i++){
+        let subArray=[]
+        for(let j=0;j<obj.length;j++){
+            if()
+        }
+    }
+}
 
 module.exports = removeDuplicates;
