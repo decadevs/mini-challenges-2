1a86bbe3649c8605cfc99bc8d4a3d2976aa191ce
Mercyogbenjuwa <99675525+Mercyogbenjuwa@users.noreply.github.com>
2022-09-13 15:37:14 +0100

    Update index.js

--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,20 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text) {
+if (text === '')return ''
+  if (typeof text !== 'string')throw new Error ('please provide a morse string')
+  const input = text.trim ()
+  const words = input.split (/\s{3,}/)
+
+  let result = ''
+  words.forEach ((word) => {
+  words.split('').forEach((char) =>{
+    result += MORSE-CODE [char]
+  })
+  result += ''
+})
+return result . trim ()
+}
 
 module.exports = morse;
