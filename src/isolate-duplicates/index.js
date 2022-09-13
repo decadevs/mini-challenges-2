63ba0bd27ac235817170055b640c7e798ab1f629
Decagon <decagon@Decagons-MacBook-Air.local>
2022-09-13 02:24:53 +0100

    First commit

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,26 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    // let textStr = text.split('')
+    // let newText = []
+    // console.log(textStr)
+    // for(let i = 0; i < textStr.length; i++){
+    //     console.log(textStr[i].match(/a/g))
 
+    let newText = []
+    let arrayFromstr = text.split("");
+    let unique = Array.from(new Set(arrayFromstr));
+        console.log(unique)
+
+        for(let i = 2; i < unique.length; i++){
+            console.log(unique[i])
+
+            if(unique.length > 2){
+                newText.push(unique[i])
+                console.log(newText)
+            }
+        }
+return `${unique} ${newText}`
+   }
+
+
+console.log(isolateDuplicates("aaaaaabbcdefffffg"))
 module.exports = isolateDuplicates;
