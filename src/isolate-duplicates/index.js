31cf95af76f56771fc7ad3a56757e31d596c747e
Decagon <decagon@Decagons-MacBook-Air.local>
2022-09-13 12:45:53 +0100

    completed mmini challenge 2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,35 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    if (arguments.length < 1 || typeof text != "string") {
+        throw "Please enter a valid string";
+      }
+      const firstOutput = [];
+      let count = 0;
+      let secondOutput = 0;
+      text = text.split("");
+      for (let i = 0; i < text.length; i++) {
+        firstOutput.push(text[i]);
+        if (
+          text[i + 1] != undefined &&
+          text[i].toLowerCase() == text[i + 1].toLowerCase()
+        ) {
+          count++;
+          if (count == 2) {
+            firstOutput.push("[");
+            secondOutput++;
+          }
+        } else if (
+          (text[i + 1] != undefined &&
+            text[i].toLowerCase() !== text[i + 1].toLowerCase()) ||
+          i == text.length - 1
+        ) {
+          if (count > 1) {
+            firstOutput.push("]");
+          }
+          count = 0;
+        }
+      }
 
-module.exports = isolateDuplicates;
+      return [firstOutput.join(""), secondOutput];
+}
+
+module.exports = isolateDuplicates;      
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,22 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    let objKeys = Object.keys(obj)
+    let sortedKeys = objKeys.sort((a,b) => b-a);
+    let removeDuplicatesObj = {};
+    let letterChecked = {};
+    //This loop through the input object
+    for(let i =0; i < sortedKeys.length; i++) {
+        let letterArr = [];
+        // This loops through the value of the object i.e arrays
+        for(let j=0; j < obj[sortedKeys[i]].length; j++) {
+            if(!letterChecked[obj[sortedKeys[i]][j]]){
+                letterChecked[obj[sortedKeys[i]][j]] = true;
+                letterArr.push(obj[sortedKeys[i]][j])
+            }
+        }
+        removeDuplicatesObj[sortedKeys[i]] = letterArr;
+    }
+
+    return removeDuplicatesObj;
+}
 
 module.exports = removeDuplicates;
