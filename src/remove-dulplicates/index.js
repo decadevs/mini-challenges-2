d5361600465d23a1de42f4180f116ed12a3729ec
Victor-Olufade <96873981+Victor-Olufade@users.noreply.github.com>
2022-09-13 17:14:49 +0100

    Update index.js

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,31 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    const arrKeys = Object.keys(obj)
+    .map(Number)
+    .sort((a,b)=>b-a)
+      
+      const sortedObj = {}
+      const sortedArrs = []
+      const eleContained = []
+      while(sortedArrs.length < arrKeys.length){
+        sortedArrs.push([])
+      }
+      
+      let count = 0
+      for(let arrays of arrKeys){
+        const arr = obj[arrays]
+        arr.forEach(ele => {
+          if(!eleContained.includes(ele)){
+            sortedArrs[count].push(ele);
+            eleContained.push(ele)
+          }
+        })
+        count += 1
+      }
+
+      for(let k = 0; k < arrKeys.length; k++){
+        sortedObj[arrKeys[k]] = sortedArrs[k]
+      }
+      return sortedObj
+}
 
 module.exports = removeDuplicates;
