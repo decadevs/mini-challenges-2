9b9b2759a3ee4cf030e144f43678f36d44bf9d28
Sofiyyah <sofiyyah.abidoye@decagon.dev>
2023-01-31 15:39:39 +0100

    Sofiyyah_Abidoye

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,28 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
 
+  if (typeof text !== 'string') {
+    throw new Error('Please enter a valid string')
+  }
+
+  let lowerText = text.toLowerCase()
+
+  let newString = ""
+  let count = 0
+
+  for (let i = 0; i < text.length; i++) {
+    newString += text[i]
+
+    if (lowerText[i] === lowerText[i - 1] && lowerText[i] === lowerText[i + 1] && lowerText[i] !== lowerText[i - 2]) {
+      newString += '['
+      count++
+    }
+
+    if (lowerText[i] === lowerText[i - 1] && lowerText[i] !== lowerText[i + 1] && lowerText[i] === lowerText[i - 2] ) {
+      newString += ']'
+    }
+  }
+  return [newString, count]
+}
+
+console.log(isolateDuplicates("aaAabbcdefffffffg"))
 module.exports = isolateDuplicates;
