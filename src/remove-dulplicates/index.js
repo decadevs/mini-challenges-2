ed6c86a735990f6f79298b73d666f779cf7241d6
Adjushi Akpevwe Favour <favour.adjushi@decagon.dev>
2023-01-31 16:08:39 +0100

    Adjusi_Akpevwe_Favour

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,14 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+  let n = [];
+  let sub = (a, b) => [...new Set([...a].filter((y) => !b.includes(y)))];
+  return Object.entries(obj)
+    .reverse()
+    .map((entry) => {
+      const subCollector = n;
+      n = [...new Set([...n, ...entry[1]])];
+      return [entry[0], [...new Set(sub(entry[1], subCollector))]];
+    })
+    .reduce((arrays, array) => ((arrays[array[0]] = array[1]), arrays), {});
+}
 
 module.exports = removeDuplicates;
