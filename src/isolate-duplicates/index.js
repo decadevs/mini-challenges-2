2ebe6fb696f577fbace82e1f6c002ea3239f2bfc
mac <pro@Pros-MacBook-Pro.local>
2023-08-15 22:09:41 +0100

    Completed mini-challenge-2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,45 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    if (typeof text !== 'string') {
+        throw new Error("Please enter a valid string");  // Create an Error object with the message
+    }
+    
+    let duplicate = {};
+    let newString = "";
+    let str = "";
+    let count = 0;
+    let stringCount = [];
+    let textNew = text.toLowerCase();
 
+
+    for (let i = 0; i < textNew.length; i++) {
+        if (newString !== textNew[i]) {
+            if (duplicate[newString] > 2) {
+                str += ']';
+            }
+            duplicate[newString] = 0;
+            newString = textNew[i];
+        }
+
+        if (!duplicate[textNew[i]]) {
+            duplicate[textNew[i]] = 1;
+        } else if (duplicate[textNew[i]] === 1) {
+            duplicate[textNew[i]]++;
+        } else if (duplicate[textNew[i]] === 2) {
+            str += '[';
+            duplicate[textNew[i]]++;
+            count++;
+        }
+
+        str += text[i];
+    }
+
+    if (duplicate[newString] > 2) {
+        str += ']';
+    }
+
+    stringCount.push(str);
+    stringCount.push(count);
+
+    return stringCount;  // Return the updated stringCount array
+}
 module.exports = isolateDuplicates;
