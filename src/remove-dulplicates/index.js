05ed320a6137cb96f5b3d861e81010148381e917
Chibike <user@MacBook-Air.local>
2023-08-16 12:59:12 +0100

    mini-challenges

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,26 @@
-function removeDuplicates(obj) {}
+
+
+function removeDuplicates(obj) {
+    
+    let compare = [],
+     newObject = {},
+     k = obj.keys(obj);
+    
+    for(let i = k.length-1; i >= 0; i--){
+        let val = obj[k[i]],
+         newVal = [];
+         
+         for(j = 0; j < val.length; j++){
+            if(!compare.includes(val[j])){
+                compare.push(val[j]);
+            newVal.push(val[j]);
+            
+              } 
+          }
+          newObj[key[i]] = newVal;
+        }
+       return newObject;
+      }
+    
 
 module.exports = removeDuplicates;
