595c099b1f3e44f7074776809d5b7341183b438f
ANYIDOLLAR <anyidollar@wikiwoos-MacBook-Air.local>
2023-08-16 11:09:45 +0100

    my second mini-challenge-2

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,28 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
 
+  let compare = []
+  let result = {}
+  let key = Object.keys(obj)
+  let value = Object.values(obj)
+  console.log(value)
+
+
+  
+  for(let i = key.length-1 ; i>= 0; i--){
+      let value = obj[key[i]]
+      let newVal = []
+          value.forEach(item =>{
+              if(!compare.includes(item)){
+                  compare.push(item)
+                  newVal.push(item)
+  
+              }
+          })
+          result[key[i]] = newVal
+  
+   
+  }
+  return result
+}  
+  
 module.exports = removeDuplicates;
